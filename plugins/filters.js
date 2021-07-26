090b6b38a4fa792c0a7fa04a325935a0729b6db1
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-07-26 18:09:32 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2020 farhan-dqz.
+/* Copyright (C) 2020 TERROR.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 WhatsAsena - Yusuf Usta
