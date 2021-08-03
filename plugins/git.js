8c9fa31f8374d50de9c8a2a2aedfa014c6a371f1
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-03 10:08:27 +0530

    Create git.js

---
+++ plugins/git.js
@@ -0,0 +1,63 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "https://avatars.githubusercontent.com/u/85664936?v=4";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴋᴀʀᴛʜɪᴋ_ᴛᴇʀʀᴏʀ_ʙᴏʏ*
+
+*owner number wa.me/919778042644*
+
+*github link       _https://github.com/terror-boy/WhiteDevil_*
+
+*audio commads    _https://github.com/terror-boy/WhiteDevil/tree/master/uploads_*
+
+*sticker commads  _https://github.com/terror-boy/WhiteDevil/tree/master/stickers_*
+`}) 
+
+}));";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴋᴀʀᴛʜɪᴋ_ᴛᴇʀʀᴏʀ_ʙᴏʏ*
+
+*owner number wa.me/919778042644*
+
+*github link       _https://github.com/terror-boy/WhiteDevil_*
+
+*audio commads    _https://github.com/terror-boy/WhiteDevil/tree/master/uploads_*
+
+*sticker commads  _https://github.com/terror-boy/WhiteDevil/tree/master/stickers_*
+`})  
+
+}));
