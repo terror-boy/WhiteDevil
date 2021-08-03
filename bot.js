4237d9bdaadf599691af1b9025788c70417b94d3
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-03 14:59:09 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,8 +69,8 @@ async function whatsAsena () {
     });
     
     const conn = new WAConnection();
-    const Session = new StringSession();
     conn.version = [2, 2123, 8];
+    const Session = new StringSession();
    
     conn.sendMessage(conn.user.jid, '*===== [ 📢 Announcement 📢 ] =====*\n\n👋 Enthokke und machane Sukhamalle 😂\n\n💖 Thank You For Using White Devil 💖', MessageType.text)
 
