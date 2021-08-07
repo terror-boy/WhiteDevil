ff71ef2bc8314cdfd18da7bfeba11ddb0b739ca2
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-07 07:54:02 +0530

    Create antispam.js

---
+++ plugins/antispam.js
@@ -0,0 +1,71 @@
+const Asena = require('../events');
+
+const {MessageType} = require('@adiwajshing/baileys');
+
+const Ln = ('This command for any emergency situation about any kind of WhatsApp SPAM in Group');
+
+const code = ('A\nɴ\nᴛ\nɪ\ns\nᴘ\nᴀ\nᴍ\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n *🎭WHITE DEVIL*')
+const FINAL = "THIS IS AN ANTISAPM (anti lag),\nThis code prevents your phone from getting stuck due to malicious messages"
+const TMUTE = "Trying to close temporary,\nAttempting to temporarily close the group"
+const MUT = ".mute 1d"
+const SCXR = "Running script...."
+Asena.addCommand({pattern: 'antispm', fromMe: true, deleteCommand: false, desc: Ln,}, (async (message, match) => {
+
+  var msg = await message.reply('Preforming....');
+
+  await message.client.sendMessage(
+    message.jid,TMUTE, MessageType.text);
+
+    await message.client.sendMessage(
+      message.jid,MUT, MessageType.text);
+
+      await message.client.sendMessage(
+        message.jid,SCXR, MessageType.text);
+
+        await message.client.sendMessage(
+          message.jid,code, MessageType.text);
+
+    await message.client.sendMessage(
+        message.jid,code, MessageType.text);
+
+        await message.client.sendMessage(
+            message.jid,code, MessageType.text);
+
+            await message.client.sendMessage(
+                message.jid,code, MessageType.text);
+
+                await message.client.sendMessage(
+                  message.jid,code, MessageType.text);
+
+                  await message.client.sendMessage(
+                    message.jid,code, MessageType.text);
+
+                    await message.client.sendMessage(
+                      message.jid,code, MessageType.text);
+
+                      await message.client.sendMessage(
+                        message.jid,code, MessageType.text);
+  
+                               await message.client.sendMessage(
+            message.jid,code, MessageType.text);
+
+            await message.client.sendMessage(
+                message.jid,code, MessageType.text);
+
+                await message.client.sendMessage(
+                  message.jid,code, MessageType.text);
+
+                  await message.client.sendMessage(
+                    message.jid,code, MessageType.text);
+
+                    await message.client.sendMessage(
+                      message.jid,code, MessageType.text);
+
+                      await message.client.sendMessage(
+                        message.jid,code, MessageType.text);
+  
+                          await message.client.sendMessage(
+                             message.jid,FINAL, MessageType.text);
+
+
+}));
