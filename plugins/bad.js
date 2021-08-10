54d07ecc9e7cc3386961dc7a2f52e7416319e57e
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-10 10:34:28 +0530

    Update bad.js

--- plugins/bad.js
+++ plugins/bad.js
@@ -153,5 +153,5 @@ Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (mess
             if (us) return;
             await message.client.groupRemove(message.jid, [message.data.participant]);         
             await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
-     
-}));
+       }
+));
