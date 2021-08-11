71c781e708224247efb9ad6b1b9f34a4fc4ae9b8
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-11 13:31:16 +0530

    Update music.js

--- plugins/music.js
+++ plugins/music.js
@@ -9,9 +9,9 @@ const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 
 const Language = require('../language');
-const Lang = Language.getString('wallpaper');
+const Lang = Language.getString('music');
 
-Asena.addCommand({pattern: 'music', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'music', fromMe: false, desc: Lang.MC}, (async (message, match) => {
 
     var r_text = new Array ();
     
