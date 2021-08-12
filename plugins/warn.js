333266bfbe3b8634bd53c190c53dfcc21214a091
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-12 12:24:37 +0530

    Update warn.js

--- plugins/warn.js
+++ plugins/warn.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('gitlink');
 
-Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message, match) => {
+Asena.addCommand({pattern: 'warn', fromMe: false, desc: Lang.WN}, (async (message, match) => {
 
     var r_text = new Array ();
     
