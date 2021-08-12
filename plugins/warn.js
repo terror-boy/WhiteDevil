3368174a96e328259a111a1dabd2e00e0f3fac2c
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-12 10:27:05 +0530

    test

--- plugins/warn.js
+++ plugins/warn.js
@@ -6,14 +6,9 @@ const Language = require('../language');
 const Lang = Language.getString('gitlink');
 
 
-Asena.addCommand({pattern: 'warn', fromMe: false, desc: Lang.WN}, (async (message, match) => { 
-
-   await message.client.blockUser(message.reply_message.jid, "remove"); await message.client.sendMessage(message.jid, '@' + message.reply_message.jid.split('@')[0] + '```,MessageType.text, { quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
-    
-                                                                                                         
-                                                                                                         
-
-    
+Asena.addCommand({pattern: 'warn', fromMe: false, desc: Lang.WN}, (async (message, match) => { await message.client.blockUser(message.reply_message.jid, "remove"); await message.client.sendMessage(message.jid, '@' + message.reply_message.jid.split('@')[0] + '```,MessageType.text, { quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
+  
+   
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
