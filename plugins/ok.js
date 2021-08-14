c3e2f0ed46f0892f63b208c6fcce9b53515151af
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-14 17:20:19 +0530

    Update ok.js

--- plugins/ok.js
+++ plugins/ok.js
@@ -3,9 +3,9 @@ const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 
 const Language = require('../language');
-const Lang = Language.getString('ok');
+const Lang = Language.getString('music');
 
-Asena.addCommand({pattern: 'ok', fromMe: false, desc: Lang.OK}, (async (message, match) => {
+Asena.addCommand({pattern: 'music', fromMe: false, desc: Lang.MC}, (async (message, match) => {
 
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `!ok
