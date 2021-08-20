b6a28426f69e05a15fec7989a0746c833f5685a6
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-20 20:46:19 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -10,15 +10,17 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
     var r_text = new Array ();
     
     
-    r_text[0] = "https://avatars.githubusercontent.com/u/85664936?v=4";
+     r_text[1] = "https://avatars.githubusercontent.com/u/85664936?v=4";
+     r_text[2] = "https://camo.githubusercontent.com/15cd035329b5b86f2908acfcdc39db031e4e167b1445aba7ff5a19f7c7b19933/68747470733a2f2f6361726469766f2e76657263656c2e6170702f6170693f6e616d653d4b41525448494b5f544552524f522d253230424f59266465736372697074696f6e3d48692c25323049276d253230612532306d6f646572617465253230446576656c6f70657225323025463025394625393825384526696d6167653d68747470733a2f2f692e696d6775722e636f6d2f4e796f54674a452e706e676261636b67726f756e64436f6c6f723d253233656366306631266769746875623d746572726f722d626f7926267061747465726e3d6c65616626636f6c6f725061747465726e3d253235656165616561.jpg";
+   
     
     
-    var i = Math.floor(1*Math.random())
+    var i = Math.floor(3*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━✨️Ameer Suhail✨️━━━━━⛦
-𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 Karthik_terror-boy 𝐛𝐨𝐭.
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━✨️Karthik_terror-boy✨️━━━━━⛦
+𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 Karthik_terror-boy's 𝐛𝐨𝐭.
             *WhiteDevil*
 ■□■□■□■□■□■□■□■□■□■□
 ♕ *𝙈𝙚𝙙𝙞𝙖 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♕
@@ -57,7 +59,7 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
 🍙𝙐𝙨𝙚➜ Finds the text written on the image.
 ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -wallpaper
 🍙𝙐𝙨𝙚➜ It sends you random wallpaper.
-■□■□■□■□■□Pikachu■□■□■□■□■□
+■□■□■□■□■□WhiteDevil■□■□■□■□■□
 ♟ *𝙁𝙪𝙣 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♟
 ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -joke 
 🍙𝙐𝙨𝙚➜ It sends a random joke.
@@ -88,7 +90,7 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
 ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -anime <text>
 🍙𝙐𝙨𝙚➜  Gives you info about anime.
 ⚠️anime whitedevil
-■□■□■□■□■Pikachu□■□■□■□■□■□
+■□■□■□■□■WhiteDevil□■□■□■□■□■□
 ♝ *𝙎𝙩𝙞𝙘𝙠𝙚𝙧 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♝
 ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -sticker 
 🍙𝙐𝙨𝙚➜ Converts img/gif into a sticker.
