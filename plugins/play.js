faeed44f496d569cb6301ea1a2ff07845e249930
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-21 19:47:20 +0530

    Create play.js

---
+++ plugins/play.js
@@ -0,0 +1,24 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('gitlink');
+
+Asena.addCommand({pattern: 'play', fromMe: false, desc: Lang.GL}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "https://i.imgur.com/kvX50Ch.png";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*PLEASE TYPE  [^]musics* \n\n\n\n\n *[^]=handlers like {!,./}* \n\n _CODED BY KARTHIK-TERROR BOY_
+`}) 
+
+}));
