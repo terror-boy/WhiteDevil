5e9aaacdf81e7514b6b0d958fc1c4952f24cd2b8
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-22 11:45:33 +0530

    Add files via upload

---
+++ plugins/groupclose.js
@@ -0,0 +1,36 @@
+/* Copyright (C) 2021 TERROR BOY.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+WhiteDevil - TERROR BOY
+*/
+
+const Asena = require('../events');
+
+const {MessageType} = require('@adiwajshing/baileys');
+
+const CL = ('This command for Close the  Group at night');
+
+const code = ('!mute 10h')
+const FINAL = "THIS GROUP IS CLOSED & OPEN IN MORNING"
+const TMUTE = "Trying to close temporary,\nAttempting to temporarily close the group"
+const SCXR = "Running script...."
+Asena.addCommand({pattern: 'close', fromMe: true, deleteCommand: false, desc: CL,}, (async (message, match) => {
+
+  var msg = await message.reply('Preforming....');
+
+  await message.client.sendMessage(
+    message.jid,TMUTE, MessageType.text);
+
+      await message.client.sendMessage(
+        message.jid,SCXR, MessageType.text);
+
+        await message.client.sendMessage(
+          message.jid,code, MessageType.text);
+
+                          await message.client.sendMessage(
+                             message.jid,FINAL, MessageType.text);
+
+
+}));
---
+++ plugins/groupopen.js
@@ -0,0 +1,36 @@
+/* Copyright (C) 2021 TERROR BOY.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+WhiteDevil - TERROR BOY
+*/
+
+const Asena = require('../events');
+
+const {MessageType} = require('@adiwajshing/baileys');
+
+const OP = ('This command for OPEN the  Group at MORNING');
+
+const code = ('!unmute')
+const FINAL = "```The group  has open 🔓!```"
+const TMUTE = "Trying to open the group"
+const SCXR = "Running script...."
+Asena.addCommand({pattern: 'open', fromMe: true, deleteCommand: false, desc: OP,}, (async (message, match) => {
+
+  var msg = await message.reply('Preforming....');
+
+  await message.client.sendMessage(
+    message.jid,TMUTE, MessageType.text);
+
+      await message.client.sendMessage(
+        message.jid,SCXR, MessageType.text);
+
+        await message.client.sendMessage(
+          message.jid,code, MessageType.text);
+
+                          await message.client.sendMessage(
+                             message.jid,FINAL, MessageType.text);
+
+
+}));
