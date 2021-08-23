aaf9e76ea360f11760c112a065f065394f157b38
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-23 21:54:24 +0530

    Update StringSession.js

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2021 TERROR-BOY 
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+WhiteDevil - TERROR-BOY 
 */
 
 const fs = require('fs');
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('ASENA_SESSION' in process.env && string === undefined) {
+        if ('WHITE_SESSION' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'ASENA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'WhiteDevil;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
