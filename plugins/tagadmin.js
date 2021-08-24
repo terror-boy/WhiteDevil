a15d2fca1f0ad9e382be456a9ea90ee53f4fbf74
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-24 12:59:42 +0530

    Update tagadmin.js

--- plugins/tagadmin.js
+++ plugins/tagadmin.js
@@ -38,3 +38,18 @@ else if (Config.WORKTYPE == 'public') {
         await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
     }));
 }
+
+else if (Config.WORKTYPE == 'admin') {
+    Asena.addCommand({pattern: 'tagadmin$', fromMe: false, desc: Lang.TAGADMİN}, (async (message, match) => {
+        let grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(async (uye) => {
+            if (uye.isAdmin) {
+                mesaj += '👮‍♂️@' + uye.id.split('@')[0] + '\n';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        });
+        await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {sendEphemeral: 'chat',contextInfo: {mentionedJid: jids}, previewType: 0})
+    }));
+}
