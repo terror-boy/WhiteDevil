ee2e32afa7e16db421b13eb21de46059e2adb6da
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-24 13:21:44 +0530

    Update greetings.js

--- plugins/greetings.js
+++ plugins/greetings.js
@@ -9,9 +9,17 @@ WhiteDevil TERROR BOY
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const sql = require('./sql/greetings');
+const Config = require('../config');
+
+
+const Language = require('../language');
+const Config = require('../config');
+
 
 const Language = require('../language');
 const Lang = Language.getString('greetings');
+
+if (Config.WORKTYPE == 'private') {
 
 Asena.addCommand({pattern: 'welcome$', fromMe: true, desc: Lang.WELCOME_DESC}, (async (message, match) => {
     var hg = await sql.getMessage(message.jid);
@@ -50,6 +58,91 @@ Asena.addCommand({pattern: 'goodbye (.*)', fromMe: true, dontAddCommandList: tru
         return await message.client.sendMessage(message.jid,Lang.GOODBYE_SETTED,MessageType.text)
     }
 }));
+} 
+
+else if (Config.WORKTYPE == 'public') {
+
+
+Asena.addCommand({pattern: 'welcome$', fromMe: true, desc: Lang.WELCOME_DESC}, (async (message, match) => {
+    var hg = await sql.getMessage(message.jid);
+    if (hg === false) {
+        await message.client.sendMessage(message.jid,Lang.NOT_SET_WELCOME,MessageType.text);
+    } else {
+        await message.client.sendMessage(message.jid,Lang.WELCOME_ALREADY_SETTED + hg.message + '```',MessageType.text);
+    }
+}));
+
+Asena.addCommand({pattern: 'welcome (.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+    if (match[1] === '') {
+        return await message.client.sendMessage(message.jid,Lang.NEED_WELCOME_TEXT);
+    } else {
+        if (match[1] === 'delete') { await message.client.sendMessage(message.jid,Lang.WELCOME_DELETED,MessageType.text); return await sql.deleteMessage(message.jid, 'welcome'); }
+        await sql.setMessage(message.jid, 'welcome', match[1].replace(/#/g, '\n'));
+        return await message.client.sendMessage(message.jid,Lang.WELCOME_SETTED,MessageType.text)
+    }
+}));
+
+Asena.addCommand({pattern: 'goodbye$', fromMe: true, desc: Lang.GOODBYE_DESC}, (async (message, match) => {
+    var hg = await sql.getMessage(message.jid, 'goodbye');
+    if (hg === false) {
+        await message.client.sendMessage(message.jid,Lang.NOT_SET_GOODBYE,MessageType.text)
+    } else {
+        await message.client.sendMessage(message.jid,Lang.GOODBYE_ALREADY_SETTED + hg.message + '```',MessageType.text);
+    }
+}));
+
+Asena.addCommand({pattern: 'goodbye (.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+    if (match[1] === '') {
+        return await message.client.sendMessage(message.jid,Lang.NEED_GOODBYE_TEXT,MessageType.text);
+    } else {
+        if (match[1] === 'delete') { await message.client.sendMessage(message.jid,Lang.GOODBYE_DELETED,MessageType.text); return await sql.deleteMessage(message.jid, 'goodbye'); }
+        await sql.setMessage(message.jid, 'goodbye', match[1].replace(/#/g, '\n'));
+        return await message.client.sendMessage(message.jid,Lang.GOODBYE_SETTED,MessageType.text)
+    }
+}));
+} 
+
+else if (Config.WORKTYPE == 'public') {
+
+
+Asena.addCommand({pattern: 'welcome$', fromMe: true, desc: Lang.WELCOME_DESC}, (async (message, match) => {
+    var hg = await sql.getMessage(message.jid);
+    if (hg === false) {
+        await message.client.sendMessage(message.jid,Lang.NOT_SET_WELCOME,MessageType.text);
+    } else {
+        await message.client.sendMessage(message.jid,Lang.WELCOME_ALREADY_SETTED + hg.message + '```',MessageType.text);
+    }
+}));
+
+Asena.addCommand({pattern: 'welcome (.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+    if (match[1] === '') {
+        return await message.client.sendMessage(message.jid,Lang.NEED_WELCOME_TEXT);
+    } else {
+        if (match[1] === 'delete') { await message.client.sendMessage(message.jid,Lang.WELCOME_DELETED,MessageType.text); return await sql.deleteMessage(message.jid, 'welcome'); }
+        await sql.setMessage(message.jid, 'welcome', match[1].replace(/#/g, '\n'));
+        return await message.client.sendMessage(message.jid,Lang.WELCOME_SETTED,MessageType.text)
+    }
+}));
+
+Asena.addCommand({pattern: 'goodbye$', fromMe: true, desc: Lang.GOODBYE_DESC}, (async (message, match) => {
+    var hg = await sql.getMessage(message.jid, 'goodbye');
+    if (hg === false) {
+        await message.client.sendMessage(message.jid,Lang.NOT_SET_GOODBYE,MessageType.text)
+    } else {
+        await message.client.sendMessage(message.jid,Lang.GOODBYE_ALREADY_SETTED + hg.message + '```',MessageType.text);
+    }
+}));
+
+Asena.addCommand({pattern: 'goodbye (.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+    if (match[1] === '') {
+        return await message.client.sendMessage(message.jid,Lang.NEED_GOODBYE_TEXT,MessageType.text);
+    } else {
+        if (match[1] === 'delete') { await message.client.sendMessage(message.jid,Lang.GOODBYE_DELETED,MessageType.text); return await sql.deleteMessage(message.jid, 'goodbye'); }
+        await sql.setMessage(message.jid, 'goodbye', match[1].replace(/#/g, '\n'));
+        return await message.client.sendMessage(message.jid,Lang.GOODBYE_SETTED,MessageType.text)
+    }
+}));
+}
 
 if (Config.WORKTYPE == 'admin') {
 
