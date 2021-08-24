5ba330b348326dca252856480fb333ce320225b7
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-24 12:55:08 +0530

    new work type added

--- plugins/addmin.js
+++ plugins/addmin.js
@@ -5,7 +5,7 @@ you may not use this file except in compliance with the License.
 
 WhiteDevil- terror-boy
 
-codded by afnanplk
+    codded by afnanplk
 */
 
 const {MessageType, GroupSettingChange} = require('@adiwajshing/baileys');
