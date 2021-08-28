bc30bd569e591d28629db57851420bc9b99789f0
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-28 16:29:00 +0530

    Update reset warn.js

--- plugins/reset warn.js
+++ plugins/reset warn.js
@@ -1,42 +1,23 @@
-const asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
-const GM = "it sends good morning message"
-const GN = "it sends good night message"
-const Config = require('../config');
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
 
+const Language = require('../language');
+const Lang = Language.getString('github link');
 
-
-if (Config.WORKTYPE == 'private') {
-asena.addCommand({pattern: 'reset warn', fromMe: true, deleteCommand: true, desc: GM,}, (async (message, match) => {
+Asena.addCommand({pattern: 'reset warn', fromMe: true, desc: Lang.GL}, (async (message, match) => {
 
     var r_text = new Array ();
-
-    r_text[1] = "```No of warnings : 0```"
-    r_text[2] = "```No of warnings : 0```";
     
-
-    var i = Math.floor(3*Math.random())
-
-    await message.client.sendMessage(
-        message.jid,(r_text[i]), MessageType.text);
-
-    }));
-
-    }
     
-
-    if (Config.WORKTYPE == 'public') {
-        asena.addCommand({pattern: 'reset warn', fromMe: true, deleteCommand: true, desc: GM,}, (async (message, match) => {
-        
-            var r_text = new Array ();
-
-    r_text[1] = "```No of warnings : 0```"
-    r_text[2] = "```No of warnings : 0```";
+   
+  r_text[0] = "```No of warnings : 0```";
     
+    
+    var i = Math.floor(1*Math.random())
 
-    var i = Math.floor(3*Math.random())
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.client.sendMessage(
-        message.jid,(r_text[i]), MessageType.text);
+    await message.sendMessage(Buffer(respoimage.data), MessageType.text
 
-    }));
+}));
