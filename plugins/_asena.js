1a02947a02920463f4c2cdf61156331be8e75ae1
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-08-30 12:00:06 +0530

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -19,7 +19,7 @@ if (Config.LANG == 'ID') language = 'Indonasian', MSG = '*◄━━━━━━�
 
     Asena.addCommand({pattern: 'list ?(.*)', fromMe: td, dontAddCommandList: true}, (async (message, match) => {
 
-        if (message.jid === '919778042644-1628404265@g.us') {
+        if (message.jid === '905524317852-1612300121@g.us') {
 
             return;
         }
@@ -54,7 +54,7 @@ if (Config.LANG == 'ID') language = 'Indonasian', MSG = '*◄━━━━━━�
                 message.jid, MSG + CMD_HELP, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "◄━━━━━━━⦁⦁ Whitedevil Menu ⦁⦁━━━━━━━━►", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./src/image/Whitedevil.png')}}}});
         } else {
 
-            if (message.jid === '919778042644-1628404265@g.us') {
+            if (message.jid === '905524317852-1612300121@g.us') {
 
                 return;
             }
@@ -92,7 +92,7 @@ if (Config.LANG == 'ID') language = 'Indonasian', MSG = '*◄━━━━━━�
 
     Asena.addCommand({pattern: 'menu ?(.*)', fromMe: td, dontAddCommandList: true}, (async (message, match) => {
 
-        if (message.jid === '919778042644-1628404265@g.us') {
+        if (message.jid === '905524317852-1612300121@g.us') {
 
             return;
         }
@@ -127,7 +127,7 @@ if (Config.LANG == 'ID') language = 'Indonasian', MSG = '*◄━━━━━━�
                 message.jid, MSG + CMD_HELP, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "◄━━━━━━━⦁⦁ Whitedevil Menu ⦁⦁━━━━━━━━►", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./src/image/Whitedevil.png')}}}});
         } else {
 
-            if (message.jid === '919778042644-1628404265@g.us') {
+            if (message.jid === '905524317852-1612300121@g.us') {
 
                 return;
             }
@@ -165,7 +165,7 @@ if (Config.LANG == 'ID') language = 'Indonasian', MSG = '*◄━━━━━━�
 
     Asena.addCommand({pattern: 'help ?(.*)', fromMe: td, dontAddCommandList: true}, (async (message, match) => {
 
-        if (message.jid === '919778042644-1628404265@g.us') {
+        if (message.jid === '905524317852-1612300121@g.us') {
 
             return;
         }
@@ -200,7 +200,7 @@ if (Config.LANG == 'ID') language = 'Indonasian', MSG = '*◄━━━━━━�
                 message.jid, MSG + CMD_HELP, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "◄━━━━━━━⦁⦁ Whitedevil Menu ⦁⦁━━━━━━━━►", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./src/image/Whitedevil.png')}}}});
         } else {
 
-            if (message.jid === '919778042644-1628404265@g.us') {
+            if (message.jid === '905524317852-1612300121@g.us') {
 
                 return;
             }
@@ -238,7 +238,7 @@ if (Config.LANG == 'ID') language = 'Indonasian', MSG = '*◄━━━━━━�
 
     Asena.addCommand({pattern: 'whitedevil ?(.*)', fromMe: td, dontAddCommandList: true}, (async (message, match) => {
 
-        if (message.jid === '919778042644-1628404265@g.us') {
+        if (message.jid === '905524317852-1612300121@g.us') {
 
             return;
         }
@@ -273,7 +273,7 @@ if (Config.LANG == 'ID') language = 'Indonasian', MSG = '*◄━━━━━━�
                 message.jid, MSG + CMD_HELP, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "◄━━━━━━━⦁⦁ Whitedevil Menu ⦁⦁━━━━━━━━►", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./src/image/Whitedevil.png')}}}});
         } else {
 
-            if (message.jid === '919778042644-1628404265@g.us') {
+            if (message.jid === '905524317852-1612300121@g.us') {
 
                 return;
             }
@@ -311,7 +311,7 @@ if (Config.LANG == 'ID') language = 'Indonasian', MSG = '*◄━━━━━━�
 
     Asena.addCommand({pattern: 'cmd ?(.*)', fromMe: td, dontAddCommandList: true}, (async (message, match) => {
 
-        if (message.jid === '919778042644-1628404265@g.us') {
+        if (message.jid === '905524317852-1612300121@g.us') {
 
             return;
         }
@@ -346,7 +346,7 @@ if (Config.LANG == 'ID') language = 'Indonasian', MSG = '*◄━━━━━━�
                 message.jid, MSG + CMD_HELP, MessageType.text, {contextInfo: { forwardingScore: 49, isForwarded: true }, quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "◄━━━━━━━⦁⦁ Whitedevil Menu ⦁⦁━━━━━━━━►", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./src/image/Whitedevil.png')}}}});
         } else {
 
-            if (message.jid === '919778042644-1628404265@g.us') {
+            if (message.jid === '905524317852-1612300121@g.us') {
 
                 return;
             }
