1cb06c248b0ec96ab3c5bfe323b67dad02151ad8
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-01 12:08:32 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,51 +1,18 @@
-const Asena = require('../events');
-const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
-const axios = require('axios');
-
-const Language = require('../language');
-const Lang = Language.getString('gitlink');
-
-Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message, match) => {
-
-    var r_text = new Array ();
-    
-    
-   
-  r_text[0] = "https://avatars.githubusercontent.com/u/85664936?v=4";
-    
-    
-    var i = Math.floor(1*Math.random())
-
-    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
-
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴋᴀʀᴛʜɪᴋ_ᴛᴇʀʀᴏʀ_ʙᴏʏ*
-
- *owner number wa.me/919778042644*
- 
-
- *ABOUT ME          _https://github.com/terror-boy_*
- 
- 
- 
- *Github link       _https://github.com/terror-boy/WhiteDevil_*
- 
- 
- 
-
- *Audio commads-1    _https://github.com/terror-boy/White/tree/master/uploads_*
- 
- 
- 
-  *Audio commads-2    _https://github.com/terror-boy/White/tree/master/upload_*
-  
-  
- 
-
- *Sticker commads  _https://github.com/terror-boy/White/tree/master/stickers_*
-
-
-
-*Video Commands   _https://github.com/terror-boy/White/tree/master/video_*
-`}) 
-
-})); 
+/***************************************************************************/
+/*                                                                         */
+/*  This obfuscated code was created by Javascript Obfuscator Free Version.*/
+/*  Javascript Obfuscator Free Version can be downloaded here              */
+/*  http://javascriptobfuscator.com                                        */
+/*                                                                         */
+/***************************************************************************/
+const Asena=require('\x2E\x2E\x2F\x65\x76\x65\x6E\x74\x73');//0
+const {MessageType,MessageOptions,Mimetype}=require('\x40\x61\x64\x69\x77\x61\x6A\x73\x68\x69\x6E\x67\x2F\x62\x61\x69\x6C\x65\x79\x73');//1
+const axios=require('\x61\x78\x69\x6F\x73');//2
+const Language=require('\x2E\x2E\x2F\x6C\x61\x6E\x67\x75\x61\x67\x65');//4
+const Lang=Language.getString('\x67\x69\x74\x6C\x69\x6E\x6B');//5
+Asena.addCommand({pattern:'\x67\x69\x74',fromMe:false,desc:Lang.GL},(async (_0x171D5,_0x17177)=>
+{
+	var _0x17233= new Array();//9
+	_0x17233[0]= "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x61\x76\x61\x74\x61\x72\x73\x2E\x67\x69\x74\x68\x75\x62\x75\x73\x65\x72\x63\x6F\x6E\x74\x65\x6E\x74\x2E\x63\x6F\x6D\x2F\x75\x2F\x38\x35\x36\x36\x34\x39\x33\x36\x3F\x76\x3D\x34";var _0x17119=Math.floor(1* Math.random());//16
+	var _0x17291= await axios["\x67\x65\x74"](`${_0x17233[_0x17119]}`,{responseType:'\x61\x72\x72\x61\x79\x62\x75\x66\x66\x65\x72'});//18
+	 await _0x171D5.sendMessage(Buffer(_0x17291.data),MessageType.image,{mimetype:Mimetype.png,caption:`*ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴋᴀʀᴛʜɪᴋ_ᴛᴇʀʀᴏʀ_ʙᴏʏ*
