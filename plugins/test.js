a7d39906060cc0f494822c928e12b0b834205084
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-02 13:42:33 +0530

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,447 @@
+const Asena = require('../events');
+const {MessageType, Mimetype} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'enhack ?(.*)', fromMe: true}, (async (message, match) => {
+
+    if (message.reply_message === false) {
+
+        return await message.sendMessage('```OPERATION CANCEL!```\n\n_Reply to the target account’s message_');
+
+    } else
+
+        var r_text = new Array ();
+
+        r_text[0] = "*Starting Process For Chosen User!* \n\n```Python Version:``` 3.5\n```UserBot:``` Asena-Beta\n```WEB API:``` True ";
+        r_text[1] = " \n*Target Selected! Processes Starting..* \n\n```Python Version:``` 3.5\n```UserBot:``` Asena-Beta\n```WEB API:``` True ";
+        r_text[2] = " \n*User Detected!* \n\n```Python Version:``` 3.6\n```Target User:``` Mobile\n```Bot:``` False ";
+        r_text[3] = " \n*User Detected!.* \n\n```Python Version:``` 2.8\n```Target User:``` Null\n```Bot:``` False ";
+        r_text[4] = " \n*User Found!.* \n\n```Python Version:``` 3.9\n```Target User:``` Mobile\n```WEB API:``` False-Inactive ";
+        r_text[5] = " \n*Account Found!!* \n\n```Python Version:``` 2.6\n```Target User:``` Null\n```API TYPE:``` Whatsapp-Mobile/WEB ";
+        r_text[6] = " \n*Asena, Gathered Information For Your Chosen Account!* \n\n```Asena Version:``` 0.8.1-Beta\n```String Type:``` New-Anonymously\n```Repo:``` DeepWeb-Universal ";
+        r_text[7] = " \n*Gathering Information...* \n\n```UserBot Version:``` 0.9\n```Bot:``` False\n```Confirmed:``` False ";
+        r_text[8] = " \n*Gathering Information...* \n\n```UserBot Version:``` 0.9\n```Bot:``` False-Null\n```Confirmed:``` False ";
+        r_text[9] = " \n*Beniwal, Gathered Information For Your Chosen Account!* \n\n```Bot Version:``` 0.8.1-Beta\n```String Type:``` Legacy\n```Repo:``` DeepWeb-Universal-New ";
+
+        var i = Math.floor(10*Math.random())
+
+        await message.client.sendMessage(message.jid, '@' + message.reply_message.jid.split('@')[0] + ` ${r_text[i]} `, MessageType.text, {
+            quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
+
+    });
+
+    await new Promise(r => setTimeout(r, 2700));
+
+    await message.sendMessage('DATA');
+
+    await message.sendMessage('HACKING');
+
+    await new Promise(r => setTimeout(r, 3400));
+
+    var r_text = new Array ();
+
+r_text[0] = "1%";
+r_text[1] = "2%";
+r_text[2] = "3%";
+r_text[3] = "4%";
+r_text[4] = "5%";
+r_text[5] = "6%";
+r_text[6] = "7%";
+r_text[7] = "8%";
+r_text[8] = "9%";
+r_text[9] = "10%";
+
+    var i = Math.floor(10*Math.random())
+
+    var r_text2 = new Array ();
+
+r_text2[0] = "1%";
+r_text2[1] = "2%";
+r_text2[2] = "3%";
+r_text2[3] = "4%";
+r_text2[4] = "5%";
+r_text2[5] = "6%";
+r_text2[6] = "7%";
+r_text2[7] = "8%";
+r_text2[8] = "9%";
+r_text2[9] = "10%";
+
+    var a = Math.floor(10*Math.random())
+
+    var r_text3 = new Array ();
+
+r_text3[0] = "1%";
+r_text3[1] = "2%";
+r_text3[2] = "3%";
+r_text3[3] = "4%";
+r_text3[4] = "5%";
+r_text3[5] = "6%";
+r_text3[6] = "7%";
+r_text3[7] = "8%";
+r_text3[8] = "9%";
+r_text3[9] = "10%";
+
+    var b = Math.floor(10*Math.random())
+
+    var r_text4 = new Array ();
+
+r_text4[0] = "1%";
+r_text4[1] = "2%";
+r_text4[2] = "3%";
+r_text4[3] = "4%";
+r_text4[4] = "5%";
+r_text4[5] = "6%";
+r_text4[6] = "7%";
+r_text4[7] = "8%";
+r_text4[8] = "9%";
+r_text4[9] = "10%";
+
+    var c = Math.floor(10*Math.random())
+
+    var r_text5 = new Array ();
+
+r_text5[0] = "1%";
+r_text5[1] = "2%";
+r_text5[2] = "3%";
+r_text5[3] = "4%";
+r_text5[4] = "5%";
+r_text5[5] = "6%";
+r_text5[6] = "7%";
+r_text5[7] = "8%";
+r_text5[8] = "9%";
+r_text5[9] = "10%";
+
+    var d = Math.floor(10*Math.random())
+
+    await message.sendMessage(`*The Target System Hacking Now!* \n\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: ${r_text[i]} \n𝔾𝕒𝕝𝕝𝕖𝕣𝕪: ${r_text2[a]} \n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕤: ${r_text3[b]} \n𝔽𝕚𝕝𝕖𝕤: ${r_text4[c]} \nℂ𝕒𝕞𝕖𝕣𝕒: ${r_text5[d]}`);
+
+    await new Promise(r => setTimeout(r, 5000));
+
+    var r_text = new Array ();
+
+r_text[0] = "16%";
+r_text[1] = "18%";
+r_text[2] = "20%";
+r_text[3] = "22%";
+r_text[4] = "24%";
+r_text[5] = "26%";
+r_text[6] = "28%";
+r_text[7] = "30%";
+r_text[8] = "32%";
+r_text[9] = "34%";
+
+    var i = Math.floor(10*Math.random())
+
+    var r_text2 = new Array ();
+
+r_text2[0] = "12%";
+r_text2[1] = "21%";
+r_text2[2] = "18%";
+r_text2[3] = "16%";
+r_text2[4] = "25%";
+r_text2[5] = "22%";
+r_text2[6] = "29%";
+r_text2[7] = "32%";
+r_text2[8] = "11%";
+r_text2[9] = "27%";
+
+    var a = Math.floor(10*Math.random())
+
+    var r_text3 = new Array ();
+
+r_text3[0] = "13%";
+r_text3[1] = "24%";
+r_text3[2] = "34%";
+r_text3[3] = "41%";
+r_text3[4] = "15%";
+r_text3[5] = "22%";
+r_text3[6] = "28%";
+r_text3[7] = "31%";
+r_text3[8] = "29%";
+r_text3[9] = "17%";
+
+    var b = Math.floor(10*Math.random())
+
+    var r_text4 = new Array ();
+
+r_text4[0] = "11%";
+r_text4[1] = "18%";
+r_text4[2] = "14%";
+r_text4[3] = "22%";
+r_text4[4] = "27%";
+r_text4[5] = "21%";
+r_text4[6] = "33%";
+r_text4[7] = "37%";
+r_text4[8] = "24%";
+r_text4[9] = "38%";
+
+    var c = Math.floor(10*Math.random())
+
+    var r_text5 = new Array ();
+
+r_text5[0] = "12%";
+r_text5[1] = "16%";
+r_text5[2] = "14%";
+r_text5[3] = "19%";
+r_text5[4] = "22%";
+r_text5[5] = "26%";
+r_text5[6] = "29%";
+r_text5[7] = "33%";
+r_text5[8] = "31%";
+r_text5[9] = "38%";
+
+    var d = Math.floor(10*Math.random())
+
+    await message.sendMessage(`*Process Running...* \n\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: ${r_text[i]} \n𝔾𝕒𝕝𝕝𝕖𝕣𝕪: ${r_text2[a]} \n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕤: ${r_text3[b]} \n𝔽𝕚𝕝𝕖𝕤: ${r_text4[c]} \nℂ𝕒𝕞𝕖𝕣𝕒: ${r_text5[d]}`);
+
+    await new Promise(r => setTimeout(r, 5000));
+
+    var r_text = new Array ();
+
+r_text[0] = "36%";
+r_text[1] = "39%";
+r_text[2] = "41%";
+r_text[3] = "46%";
+r_text[4] = "48%";
+r_text[5] = "55%";
+r_text[6] = "52%";
+r_text[7] = "59%";
+r_text[8] = "61%";
+r_text[9] = "63%";
+
+    var i = Math.floor(10*Math.random())
+
+    var r_text2 = new Array ();
+
+r_text2[0] = "34%";
+r_text2[1] = "36%";
+r_text2[2] = "39%";
+r_text2[3] = "41%";
+r_text2[4] = "44%";
+r_text2[5] = "48%";
+r_text2[6] = "49%";
+r_text2[7] = "53%";
+r_text2[8] = "57%";
+r_text2[9] = "60%";
+
+    var a = Math.floor(10*Math.random())
+
+    var r_text3 = new Array ();
+
+r_text3[0] = "43%";
+r_text3[1] = "46%";
+r_text3[2] = "47%";
+r_text3[3] = "51%";
+r_text3[4] = "54%";
+r_text3[5] = "55%";
+r_text3[6] = "57%";
+r_text3[7] = "59%";
+r_text3[8] = "61%";
+r_text3[9] = "64%";
+
+    var b = Math.floor(10*Math.random())
+
+    var r_text4 = new Array ();
+
+r_text4[0] = "40%";
+r_text4[1] = "43%";
+r_text4[2] = "45%";
+r_text4[3] = "48%";
+r_text4[4] = "51%";
+r_text4[5] = "54%";
+r_text4[6] = "57%";
+r_text4[7] = "59%";
+r_text4[8] = "60%";
+r_text4[9] = "63%";
+
+    var c = Math.floor(10*Math.random())
+
+    var r_text5 = new Array ();
+
+r_text5[0] = "39%";
+r_text5[1] = "44%";
+r_text5[2] = "45%";
+r_text5[3] = "48%";
+r_text5[4] = "52%";
+r_text5[5] = "53%";
+r_text5[6] = "58%";
+r_text5[7] = "62%";
+r_text5[8] = "63%";
+r_text5[9] = "65%";
+
+    var d = Math.floor(10*Math.random())
+
+    await message.sendMessage(`*Deleting System’s Security System* \n\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: ${r_text[i]} \n𝔾𝕒𝕝𝕝𝕖𝕣𝕪: ${r_text2[a]} \n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕤: ${r_text3[b]} \n𝔽𝕚𝕝𝕖𝕤: ${r_text4[c]} \nℂ𝕒𝕞𝕖𝕣𝕒: ${r_text5[d]}`);
+
+    await new Promise(r => setTimeout(r, 5000));
+
+    var r_text = new Array ();
+
+r_text[0] = "65%";
+r_text[1] = "68%";
+r_text[2] = "69%";
+r_text[3] = "73%";
+r_text[4] = "76%";
+r_text[5] = "78%";
+r_text[6] = "82%";
+r_text[7] = "86%";
+r_text[8] = "89%";
+r_text[9] = "91%";
+
+    var i = Math.floor(10*Math.random())
+
+    var r_text2 = new Array ();
+
+r_text2[0] = "62%";
+r_text2[1] = "66%";
+r_text2[2] = "68%";
+r_text2[3] = "70%";
+r_text2[4] = "71%";
+r_text2[5] = "74%";
+r_text2[6] = "78%";
+r_text2[7] = "82%";
+r_text2[8] = "84%";
+r_text2[9] = "88%";
+
+    var a = Math.floor(10*Math.random())
+
+    var r_text3 = new Array ();
+
+r_text3[0] = "69%";
+r_text3[1] = "72%";
+r_text3[2] = "74%";
+r_text3[3] = "79%";
+r_text3[4] = "80%";
+r_text3[5] = "81%";
+r_text3[6] = "84%";
+r_text3[7] = "87%";
+r_text3[8] = "89%";
+r_text3[9] = "92%";
+
+    var b = Math.floor(10*Math.random())
+
+    var r_text4 = new Array ();
+
+r_text4[0] = "65%";
+r_text4[1] = "68%";
+r_text4[2] = "71%";
+r_text4[3] = "72%";
+r_text4[4] = "75%";
+r_text4[5] = "78%";
+r_text4[6] = "83%";
+r_text4[7] = "86%";
+r_text4[8] = "89%";
+r_text4[9] = "93%";
+
+    var c = Math.floor(10*Math.random())
+
+    var r_text5 = new Array ();
+
+r_text5[0] = "67%";
+r_text5[1] = "71%";
+r_text5[2] = "72%";
+r_text5[3] = "75%";
+r_text5[4] = "77%";
+r_text5[5] = "82%";
+r_text5[6] = "86%";
+r_text5[7] = "89%";
+r_text5[8] = "91%";
+r_text5[9] = "93%";
+
+    var d = Math.floor(10*Math.random())
+
+    await message.sendMessage(`*Removing System’s Locks...* \n\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: ${r_text[i]} \n𝔾𝕒𝕝𝕝𝕖𝕣𝕪: ${r_text2[a]} \n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕤: ${r_text3[b]} \n𝔽𝕚𝕝𝕖𝕤: ${r_text4[c]} \nℂ𝕒𝕞𝕖𝕣𝕒: ${r_text5[d]}`);
+
+    await new Promise(r => setTimeout(r, 5000));
+
+    var r_text = new Array ();
+
+r_text[0] = "92%";
+r_text[1] = "93%";
+r_text[2] = "94%";
+r_text[3] = "95%";
+r_text[4] = "96%";
+r_text[5] = "97%";
+r_text[6] = "98%";
+r_text[7] = "99%";
+
+    var i = Math.floor(8*Math.random())
+
+    var r_text2 = new Array ();
+
+r_text2[0] = "89%";
+r_text2[1] = "90%";
+r_text2[2] = "92%";
+r_text2[3] = "93%";
+r_text2[4] = "94%";
+r_text2[5] = "95%";
+r_text2[6] = "96%";
+r_text2[7] = "97%";
+r_text2[8] = "98%";
+r_text2[9] = "99%";
+
+    var a = Math.floor(10*Math.random())
+
+    var r_text3 = new Array ();
+
+r_text3[0] = "93%";
+r_text3[1] = "94%";
+r_text3[2] = "95%";
+r_text3[3] = "96%";
+r_text3[4] = "97%";
+r_text3[5] = "98%";
+r_text3[6] = "99%";
+
+    var b = Math.floor(7*Math.random())
+
+    var r_text4 = new Array ();
+
+r_text4[0] = "94%";
+r_text4[1] = "95%";
+r_text4[2] = "96%";
+r_text4[3] = "97%";
+r_text4[4] = "98%";
+r_text4[5] = "99%";
+
+    var c = Math.floor(6*Math.random())
+
+    var r_text5 = new Array ();
+
+r_text5[0] = "94%";
+r_text5[1] = "95%";
+r_text5[2] = "96%";
+r_text5[3] = "97%";
+r_text5[4] = "98%";
+r_text5[5] = "99%";
+
+    var d = Math.floor(6*Math.random())
+
+    await message.sendMessage(`*Connecting Target Account by Using WhatsApp WEB API* \n\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: ${r_text[i]} \n𝔾𝕒𝕝𝕝𝕖𝕣𝕪: ${r_text2[a]} \n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕤: ${r_text3[b]} \n𝔽𝕚𝕝𝕖𝕤: ${r_text4[c]} \nℂ𝕒𝕞𝕖𝕣𝕒: ${r_text5[d]}`);
+
+    await new Promise(r => setTimeout(r, 3800));
+
+    await message.sendMessage('*Connection Established!* \n\n𝕎𝕙𝕒𝕥𝕤𝔸𝕡𝕡: %100 \n𝔾𝕒𝕝𝕝𝕖𝕣𝕪: %100 \n𝔻𝕠𝕔𝕦𝕞𝕖𝕟𝕥𝕤: %100 \n𝔽𝕚𝕝𝕖𝕤: %100 \nℂ𝕒𝕞𝕖𝕣𝕒: %100');
+
+    await new Promise(r => setTimeout(r, 3800));
+
+    await message.client.sendMessage(message.jid, '@' + message.reply_message.jid.split('@')[0] + ' *Saved Targeted Account Information!* ', MessageType.text, {
+            quotedMessage: message.reply_message.data, contextInfo: {mentionedJid: [message.reply_message.jid.replace('c.us', 's.whatsapp.net')]}
+
+    });
+
+    await new Promise(r => setTimeout(r, 2700));
+
+    await message.sendMessage('*Sending LOG Documents* ');
+
+    await message.client.sendMessage(message.jid, MessageType.document, {filename: 'SCRAPER.log', mimetype: Mimetype.log});
+   
+    await new Promise(r => setTimeout(r, 2600));
+
+    await message.sendMessage('*LOG Documents Created! You Can Access It In Your Database.* ');
+
+}));
+
+Asena.addCommand({pattern: 'porthack', fromMe: true}, (async (message, match) => {
+
+    await message.sendMessage('*Codded by Rohtash Beniwal* \n💻Usage: 🇬🇧 *.enhack <reply>*\nℹ️Desc: 🇬🇧 This way you can prank your friends with port hacks.\n\n🆕 _Supports Filter!_');
+
+}));
