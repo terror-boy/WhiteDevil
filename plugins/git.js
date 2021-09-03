c4579cd7f800778682d5489376328c5cbcee95ca
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-03 23:43:10 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -9,32 +9,7 @@ Asena.addCommand({pattern:'\x67\x69\x74',fromMe:false,desc:Lang.GL},(async (_0x1
 	_0x17233[0]= "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x61\x76\x61\x74\x61\x72\x73\x2E\x67\x69\x74\x68\x75\x62\x75\x73\x65\x72\x63\x6F\x6E\x74\x65\x6E\x74\x2E\x63\x6F\x6D\x2F\x75\x2F\x38\x35\x36\x36\x34\x39\x33\x36\x3F\x76\x3D\x34";var _0x17119=Math.floor(1* Math.random());//16
 	var _0x17291= await axios["\x67\x65\x74"](`${_0x17233[_0x17119]}`,{responseType:'\x61\x72\x72\x61\x79\x62\x75\x66\x66\x65\x72'});//18
 	 await _0x171D5.sendMessage(Buffer(_0x17291.data),MessageType.image,{mimetype:Mimetype.png,caption:`*ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴋᴀʀᴛʜɪᴋ-ᴛᴇʀʀᴏʀ-ʙᴏʏ*
-*owner number wa.me/919778042644*
- 
-
- *ABOUT ME          _https://github.com/terror-boy_*
- 
- 
- 
- *Github link       _https://github.com/terror-boy/WhiteDevil_*
- 
- 
- 
-
- *Audio commads-1    _https://github.com/terror-boy/White/tree/master/uploads_*
- 
- 
- 
-  *Audio commads-2    _https://github.com/terror-boy/White/tree/master/upload_*
-  
-  
- 
-
- *Sticker commads  _https://github.com/terror-boy/White/tree/master/stickers_*
-
-
-
-*Video Commands   _https://github.com/terror-boy/White/tree/master/video_*
+*owner number wa.me/919778042644* \n\n *ABOUT ME          _https://github.com/terror-boy_* \n\n *Github link       _https://github.com/terror-boy/WhiteDevil_* \n\n *Audio commads-1    _https://github.com/terror-boy/White/tree/master/uploads_* \n\n *Audio commads-2    _https://github.com/terror-boy/White/tree/master/upload_* \n\n *Sticker commads  _https://github.com/terror-boy/White/tree/master/stickers_* \n\n *Video Commands   _https://github.com/terror-boy/White/tree/master/video_* \n\n *Bot Setting malayalam* _:_ *http://imgur.com/a/alPJ0fY.mp4*
 `}) 
 
 }));
