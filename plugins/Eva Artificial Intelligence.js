52431ff721e4d15dad13e40d50f652a367af7b8d
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-03 11:21:59 +0530

    Update Eva Artificial Intelligence.js

--- plugins/Eva Artificial Intelligence.js
+++ plugins/Eva Artificial Intelligence.js
@@ -4,7 +4,6 @@ Eva AI has more than 50 Gigabyte dataset which including neural calculator,
 wikipedia data, sentiment analysis, Instagram workflow with neural cells.
 Thanks for Brainshop.ai for a rest connection with non-ethernet interaction
 Eva database. 
- 
 Eva is a multimedia-powered artificial intelligence with its own virtual brain.
 Brainshop.ai allow access to load all external conversation for train Neural cells,
 from every user's historical conversations.
@@ -58,13 +57,13 @@ let wk = conf.WORKTYPE == 'public' ? false : true
 var vtalk_dsc = ''
 var reply_eva = ''
 if (conf.LANG == 'TR') vtalk_dsc = 'Eva sesli sohbetini başlatır.', reply_eva = '*Herhangi Bir Sesli Mesaja Yanıt Verin!*'
-if (conf.LANG == 'EN') vtalk_dsc = 'Starts to White voice chat.', reply_eva = '*Reply to Any Voice Message!*'
+if (conf.LANG == 'EN') vtalk_dsc = 'Starts to Eva voice chat.', reply_eva = '*Reply to Any Voice Message!*'
 if (conf.LANG == 'AZ') vtalk_dsc = 'Eva səsli söhbətinə başlayır.', reply_eva = '*Hər hansı bir səsli mesaja cavab verin!*'
 if (conf.LANG == 'PT') vtalk_dsc = 'Começa o bate-papo por voz de Eva.', reply_eva = '*Responder a qualquer mensagem de voz!*'
 if (conf.LANG == 'RU') vtalk_dsc = 'Запускает голосовой чат Eva.', reply_eva = '*Ответьте на любое голосовое сообщение!*'
 if (conf.LANG == 'HI') vtalk_dsc = 'Eva ध्वनि चैट प्रारंभ करता है', reply_eva = '*किसी भी ध्वनि संदेश का उत्तर दें!*'
 if (conf.LANG == 'ES') vtalk_dsc = 'Comienza con el chat de voz de Eva.', reply_eva = '*¡Responde a cualquier mensaje de voz!*'
-if (conf.LANG == 'ML') vtalk_dsc = 'White വോയ്‌സ് ചാറ്റിലേക്ക് ആരംഭിക്കുന്നു.', reply_eva = '*ഏത് വോയ്‌സ് സന്ദേശത്തിനും മറുപടി നൽകുക!*'
+if (conf.LANG == 'ML') vtalk_dsc = 'Eva വോയ്‌സ് ചാറ്റിലേക്ക് ആരംഭിക്കുന്നു.', reply_eva = '*ഏത് വോയ്‌സ് സന്ദേശത്തിനും മറുപടി നൽകുക!*'
 if (conf.LANG == 'ID') vtalk_dsc = 'Mulai obrolan suara Eva.', reply_eva = '*Balas Pesan Suara Apapun!*'
 
 const recognizeAudio = () => {
@@ -89,23 +88,40 @@ const convertToWav = file => {
         .format('wav')
         .save('output.wav')
 }
+var eva_functionality = ''
+async function eva_functionality_f() {
+    await heroku.get(baseURI + '/config-vars').then(async (vars) => {
+        eva_functionality = vars.FULL_EVA
+    });
+}
+eva_functionality_f()
 
 Asena.addCommand({on: 'text', fromMe: wk, dontAddCommandList: true, deleteCommand: false}, (async (message, match) => {
-    if (message.message.startsWith('bot') && conf.FULLEVA !== 'true') {        
+    if (message.message.startsWith('Eva') && eva_functionality !== 'true') {        
         var unique_ident = message.client.user.jid.split('@')[0]      
         let acc = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0] == 'Asena' ? '7d57838203msh0c5cf65c90a7231p13b461jsn77c8cfa55871' : '7d57838203msh0c582jak19865261js1229n77c8cfa55871'
         let aitalk_mode = message.message.includes('{normal}') ? 'raw' : 'waifu'
-        var finm = message.message.replace('bot', '').replace(' ', '')   
+        var finm = message.message.replace('Eva', '').replace(' ', '')   
         var ainame = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0]
         if (ainame !== 'Asena') return;
         var ldet = lngDetector.detect(finm)
         var trmsg = ''
-        if (ldet[0][0] !== 'english') {
-            ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+        try {
+            if (ldet[0][0] !== 'english') {
+                ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+                if ('text' in ceviri) {
+                    trmsg = ceviri.text
+                }
+            } else { trmsg = finm }
+        } catch {
+            ceviri = await translatte(finm, {
+                from: 'auto', 
+                to: 'EN'
+            });
             if ('text' in ceviri) {
                 trmsg = ceviri.text
             }
-        } else { trmsg = finm }
+        }
         var uren = encodeURI(trmsg)
         await axios.get('http://api.brainshop.ai/get?bid=157104&key=VzGieV1tp1IvxPl4&uid=' + unique_ident + '&msg=' + uren).then(async (response) => {
             var fins = ''                           
@@ -120,12 +136,12 @@ Asena.addCommand({on: 'text', fromMe: wk, dontAddCommandList: true, deleteComman
     }
 }));
 Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (message, match) => {
-        if (conf.FULLEVA == 'true' && ((!message.jid.includes('-')) || (message.jid.includes('-') && 
+        if (eva_functionality == 'true' && ((!message.jid.includes('-')) || (message.jid.includes('-') && 
             (( message.mention !== false && message.mention.length !== 0 ) || message.reply_message !== false)))) {
             if (message.jid.includes('-') && (message.mention !== false && message.mention.length !== 0)) {
                 message.mention.map(async (jid) => {
                     if (message.client.user.jid.split('@')[0] === jid.split('@')[0]) {
-                        var unique_ident = message.client.user.jid.split('@')[0]      
+                        var unique_ident = message.data.participant.split('@')[0] + 'fulleva'     
                         let acc = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0] == 'Asena' ? '7d57838203msh0c5cf65c90a7231p13b461jsn77c8cfa55871' : '7d57838203msh0c582jak19865261js1229n77c8cfa55871'
                         let aitalk_mode = message.message.includes('{normal}') ? 'raw' : 'waifu'                       
                         var ainame = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0]
@@ -133,12 +149,22 @@ Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (mess
                         var finm = message.message
                         var ldet = lngDetector.detect(finm)
                         var trmsg = ''
-                        if (ldet[0][0] !== 'english') {
-                            ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+                        try {
+                            if (ldet[0][0] !== 'english') {
+                                ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+                                if ('text' in ceviri) {
+                                    trmsg = ceviri.text
+                                }
+                            } else { trmsg = finm }
+                        } catch {
+                            ceviri = await translatte(finm, {
+                                from: 'auto', 
+                                to: 'EN'
+                            });
                             if ('text' in ceviri) {
                                 trmsg = ceviri.text
                             }
-                        } else { trmsg = finm }
+                        }
                         var uren = encodeURI(trmsg)
                         await axios.get('http://api.brainshop.ai/get?bid=157104&key=VzGieV1tp1IvxPl4&uid=' + unique_ident + '&msg=' + uren).then(async (response) => {
                             var fins = ''                           
@@ -148,25 +174,35 @@ Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (mess
                                     fins = ceviri.text
                                 }
                             } else { fins = response.data.cnt }
-                            await message.client.sendMessage(message.jid,fins, MessageType.text, {contextInfo: { forwardingScore: 100, isForwarded: true },  quoted: message.data})
+                            await message.client.sendMessage(message.jid,fins, MessageType.text, { quoted: message.data})
                         })
                     }
                 })
             } else if (message.jid.includes('-') && message.reply_message !== false) {
                 if (message.reply_message.jid.split('@')[0] === message.client.user.jid.split('@')[0]) {
-                    var unique_ident = message.client.user.jid.split('@')[0]      
+                    var unique_ident = message.data.participant.split('@')[0] + 'fulleva'     
                     let acc = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0] == 'Asena' ? '7d57838203msh0c5cf65c90a7231p13b461jsn77c8cfa55871' : '7d57838203msh0c582jak19865261js1229n77c8cfa55871'
                     var ainame = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0]
                     if (ainame !== 'Asena') return;
                     var finm = message.message
                     var ldet = lngDetector.detect(finm)
                     var trmsg = ''
-                    if (ldet[0][0] !== 'english') {
-                        ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+                    try {
+                        if (ldet[0][0] !== 'english') {
+                            ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+                            if ('text' in ceviri) {
+                                trmsg = ceviri.text
+                            }
+                        } else { trmsg = finm }
+                    } catch {
+                        ceviri = await translatte(finm, {
+                            from: 'auto', 
+                            to: 'EN'
+                        });
                         if ('text' in ceviri) {
                             trmsg = ceviri.text
                         }
-                    } else { trmsg = finm }
+                    }
                     var uren = encodeURI(trmsg)
                     await axios.get('http://api.brainshop.ai/get?bid=157104&key=VzGieV1tp1IvxPl4&uid=' + unique_ident + '&msg=' + uren).then(async (response) => {
                         var fins = ''                           
@@ -176,23 +212,33 @@ Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (mess
                                 fins = ceviri.text
                             }
                         } else { fins = response.data.cnt }
-                        await message.client.sendMessage(message.jid,fins, MessageType.text, {contextInfo: { forwardingScore: 100, isForwarded: true }, quoted: message.data}
+                        await message.client.sendMessage(message.jid,fins, MessageType.text, { quoted: message.data})
                     })
                 }
             } else {
-                var unique_ident = message.client.user.jid.split('@')[0]      
+                var unique_ident = message.data.participant.split('@')[0] + 'fulleva'   
                 let acc = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0] == 'Asena' ? '7d57838203msh0c5cf65c90a7231p13b461jsn77c8cfa55871' : '7d57838203msh0c582jak19865261js1229n77c8cfa55871'
                 var ainame = os.userInfo().homedir.split('Whats')[1].split('Duplicated/')[0]
                 if (ainame !== 'Asena') return;
                 var finm = message.message
                 var ldet = lngDetector.detect(finm)
                 var trmsg = ''
-                if (ldet[0][0] !== 'english') {
-                    ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+                try {
+                    if (ldet[0][0] !== 'english') {
+                        ceviri = await translatte(finm, {from: 'auto', to: 'EN'});
+                        if ('text' in ceviri) {
+                            trmsg = ceviri.text
+                        }
+                    } else { trmsg = finm }
+                } catch {
+                    ceviri = await translatte(finm, {
+                        from: 'auto', 
+                        to: 'EN'
+                    });
                     if ('text' in ceviri) {
                         trmsg = ceviri.text
                     }
-                } else { trmsg = finm }
+                }
                 var uren = encodeURI(trmsg)
                 await axios.get('http://api.brainshop.ai/get?bid=157104&key=VzGieV1tp1IvxPl4&uid=' + unique_ident + '&msg=' + uren).then(async (response) => {
                     var fins = ''                           
@@ -202,14 +248,14 @@ Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (mess
                             fins = ceviri.text
                         }
                     } else { fins = response.data.cnt }
-                    await message.client.sendMessage(message.jid,fins, MessageType.text, {contextInfo: { forwardingScore: 100, isForwarded: true }, quoted: message.data}
+                    await message.client.sendMessage(message.jid,fins, MessageType.text, { quoted: message.data})
                 })
             }
         }
 
 }));
-Asena.addCommand({ pattern: 'vtalk$', desc: vtalk_dsc,dontAddCommandList: true, fromMe: wk }, (async (message, match) => {
-    if (!message.reply_message) return await message.client.sendMessage(message.jid,reply_eva, MessageType.text, {contextInfo: { forwardingScore: 100, isForwarded: true }, { quoted: message.data }) 
+Asena.addCommand({ pattern: 'vtalk$', desc: vtalk_dsc, fromMe: wk }, (async (message, match) => {
+    if (!message.reply_message) return await message.client.sendMessage(message.jid,reply_eva, MessageType.text, { quoted: message.data }) 
     try {
         const file = await message.client.downloadAndSaveMediaMessage({
             key: {
@@ -262,19 +308,22 @@ var already_on = ''
 var already_off = ''
 var succ_on = ''
 var succ_off = ''
+var wr_cmd = ''
 if (conf.LANG == 'TR') {
     fulleva_dsc = 'Tam fonksiyonel Eva özelliklerini aktif eder. Hesabınızı bir chatbota dönüştürün!'
     already_on = 'Eva yapay zekası halihazırda tüm fonksiyonları etkin.'
     already_off = 'Eva yapay zekası halihazırda yarı fonksiyonel çalışıyor.'
     succ_on = 'Eva, Tam Fonksiyonel Olarak Açıldı! Lütfen Biraz Bekleyin! ✅'
     succ_off = 'Eva, Yarı Fonksiyonel Olarak Ayarlandı! Lütfen Biraz Bekleyin! ☑️'
+    wr_cmd = 'Lütfen sadece *off* veya *on* komutunu kullanın.'
 }
 if (conf.LANG == 'EN') {
-    fulleva_dsc = 'Activates full functional White features. Turn your account into a ai chatbot!'
-    already_on = 'White artificial intelligence is already fully functional.'
-    already_off = 'White artificial intelligence is currently running semi-functional.'
-    succ_on = 'White Opened Fully Functionally! Please wait a bit! ✅'
-    succ_off = 'White Set to Semi-Functional! Please wait a bit! ☑️'
+    fulleva_dsc = 'Activates full functional Eva features. Turn your account into a ai chatbot!'
+    already_on = 'Eva artificial intelligence is already fully functional.'
+    already_off = 'Eva artificial intelligence is currently running semi-functional.'
+    succ_on = 'Eva Opened Fully Functionally! Please wait a bit! ✅'
+    succ_off = 'Eva Set to Semi-Functional! Please wait a bit! ☑️'
+    wr_cmd = 'Please just use the *off* or *on* command.'
 }
 if (conf.LANG == 'AZ') {
     fulleva_dsc = 'Tam funksional Eva xüsusiyyətlərini aktivləşdirir. Hesabınızı bir chatbot halına gətirin!'
@@ -282,6 +331,7 @@ if (conf.LANG == 'AZ') {
     already_off = 'Eva AI hazırda yarı funksionaldır.'
     succ_on = 'Eva Tamamilə İşlədi! Xahiş edirəm bir az gözləyin! ✅'
     succ_off = 'Eva Yarı İşləkdir! Xahiş edirəm bir az gözləyin! ☑️'
+    wr_cmd = 'Zəhmət olmasa *off* və ya *on* əmrindən istifadə edin.'
 }
 if (conf.LANG == 'RU') {
     fulleva_dsc = 'Активирует полнофункциональные функции Eva. Превратите свой аккаунт в чат-бота!'
@@ -289,6 +339,7 @@ if (conf.LANG == 'RU') {
     already_off = 'Eva AI в настоящее время частично функционирует'
     succ_on = 'Eva открылась полностью функционально! Подождите немного! ✅'
     succ_off = 'Eva настроена на полуфункциональность! Подождите немного! ☑️'
+    wr_cmd = 'Пожалуйста, просто используйте команду *off* или *on*.'
 }
 if (conf.LANG == 'ES') {
     fulleva_dsc = 'Activa todas las funciones funcionales de Eva. ¡Convierta su cuenta en un chatbot!'
@@ -296,6 +347,7 @@ if (conf.LANG == 'ES') {
     already_off = 'Eva AI es actualmente semi-funcional.'
     succ_on = '¡Eva abrió completamente funcionalmente! ¡Por favor espere un poco! ✅'
     succ_off = '¡Eva se pone semifuncional! ¡Por favor espere un poco! ☑️'
+    wr_cmd = 'Utilice el comando *off* o *on*.'
 }
 if (conf.LANG == 'HI') {
     fulleva_dsc = 'पूरी तरह कार्यात्मक Eva सुविधाओं को सक्रिय करता है। अपने खाते को चैटबॉट में बदलें!'
@@ -303,13 +355,15 @@ if (conf.LANG == 'HI') {
     already_off = 'ईवा एआई वर्तमान में अर्ध-कार्यात्मक है'
     succ_on = 'ईवा पूरी तरह कार्यात्मक रूप से खुल गई! कृपया थोड़ी प्रतीक्षा करें! ✅'
     succ_off = 'अर्ध-कार्यात्मक करने के लिए ईवा सेट! कृपया थोड़ी प्रतीक्षा करें! ☑️'
+    wr_cmd = 'कृपया केवल *on* या *off* कमांड का प्रयोग करें'
 }
 if (conf.LANG == 'ML') {
-    fulleva_dsc = 'പൂർണ്ണമായും പ്രവർത്തനക്ഷമമായ White സവിശേഷതകൾ സജീവമാക്കുന്നു. നിങ്ങളുടെ അക്കൗണ്ട് ഒരു ചാറ്റ്ബോട്ടാക്കി മാറ്റുക!'
+    fulleva_dsc = 'പൂർണ്ണമായും പ്രവർത്തനക്ഷമമായ Eva സവിശേഷതകൾ സജീവമാക്കുന്നു. നിങ്ങളുടെ അക്കൗണ്ട് ഒരു ചാറ്റ്ബോട്ടാക്കി മാറ്റുക!'
     already_on = 'ഇവ കൃത്രിമബുദ്ധി ഇതിനകം പൂർണ്ണമായി പ്രവർത്തിക്കുന്നു.'
     already_off = 'ഇവാ AI നിലവിൽ സെമി-ഫംഗ്ഷണൽ ആണ്.'
     succ_on = 'ഇവ പൂർണ്ണമായും പ്രവർത്തനക്ഷമമായി തുറന്നു! കുറച്ച് കാത്തിരിക്കൂ! ✅'
     succ_off = 'സെമി-ഫങ്ഷണൽ ആയി ഇവാ സജ്ജമാക്കുക! കുറച്ച് കാത്തിരിക്കൂ! ☑️'
+    wr_cmd = 'ദയവായി *on* അല്ലെങ്കിൽ *off* കമാൻഡ് ഉപയോഗിക്കുക.'
 }
 if (conf.LANG == 'PT') {
     fulleva_dsc = 'Ativa recursos Eva totalmente funcionais. Transforme sua conta em um chatbot!'
@@ -317,6 +371,7 @@ if (conf.LANG == 'PT') {
     already_off = 'Eva AI está semi-funcional.'
     succ_on = 'Eva abriu totalmente funcionalmente! Por favor espere um pouco! ✅'
     succ_off = 'Eva definida como semi-funcional! Por favor espere um pouco! ☑️'
+    wr_cmd = 'Use apenas o comando *off* ou *on*'
 }
 if (conf.LANG == 'ID') {
     fulleva_dsc = 'Mengaktifkan fitur Eva yang berfungsi penuh. Ubah akun Anda menjadi chatbot!'
@@ -324,12 +379,13 @@ if (conf.LANG == 'ID') {
     already_off = 'Eva AI saat ini semi-fungsional.'
     succ_on = 'Eva Dibuka Sepenuhnya Secara Fungsional! Harap tunggu sebentar! ✅'
     succ_off = 'Eva Set ke Semi-Fungsional! Mohon tunggu sebentar! ☑️'
+    wr_cmd = 'Silakan gunakan perintah *off* atau *on*.'
 }
 
-Asena.addCommand({ pattern: 'fulleva ?(.*)', desc: fulleva_dsc, fromMe: true,dontAddCommandList: true, usage: '.fulleva on / off' }, (async (message, match) => {
-    var eva_status = `${conf.FULLEVA}`
+Asena.addCommand({ pattern: 'fulleva ?(.*)', desc: fulleva_dsc, fromMe: true, usage: '.fulleva on / off' }, (async (message, match) => {
+    
     if (match[1] == 'on') {
-        if (eva_status == 'true') {
+        if (eva_functionality == 'true') {
             return await message.client.sendMessage(message.jid, '*' + already_on + '*', MessageType.text)
         }
         else {
@@ -342,7 +398,7 @@ Asena.addCommand({ pattern: 'fulleva ?(.*)', desc: fulleva_dsc, fromMe: true,don
         }
     }
     else if (match[1] == 'off') {
-        if (eva_status !== 'true') {
+        if (eva_functionality !== 'true') {
             return await message.client.sendMessage(message.jid, '*' + already_off + '*', MessageType.text)
         }
         else {
@@ -353,5 +409,7 @@ Asena.addCommand({ pattern: 'fulleva ?(.*)', desc: fulleva_dsc, fromMe: true,don
             });
             await message.client.sendMessage(message.jid, '*' + succ_off + '*', MessageType.text)
         }
+    } else {
+        return await message.client.sendMessage(message.jid, wr_cmd, MessageType.text)
     }
 }));
