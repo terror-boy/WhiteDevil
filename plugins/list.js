ffe1ca5a51560b585309f9aa7ccddbb77b2f60cd
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-04 11:22:05 +0530

    Delete list.js

--- plugins/list.js
+++
@@ -1,9 +0,0 @@
-kjk
-<!--
-Visual Code Mobile
-Developed By Manish Nirmal
-App Available on Play Store :-
-https://play.google.com/store/apps/details?id=lk.visual.code.mobile
-YouTube :-
-https://youtube.com/ManishNirmal
--->
