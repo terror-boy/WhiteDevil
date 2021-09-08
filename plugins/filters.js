6a7d88d00306d88460e9020e8afeed14e7684399
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-08 13:15:49 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -209,7 +209,7 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
         if (!!message.mention && message.mention[0] == '919778042644@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./White/video/Whitedevil.mp4'), MessageType.video, { mimetype: Mimetype.mp4Video, quoted : message.data, ptt: true})
         }
-const array = ['Rocky bhai','Whitedevil','bot setting','Bot setting','rocky bhai','Arakkal abu','Messi','Bhasi','Bussid']
+const array = ['Whitedevil']
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
