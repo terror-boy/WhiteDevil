dd162cbd335ca4a9cdf684706123bbc44784489d
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-09 11:15:14 +0530

    Update find.js

--- plugins/find.js
+++ plugins/find.js
@@ -35,7 +35,7 @@ Asena.addCommand({pattern: 'find', fromMe: false, desc: FIND_DESC }, (async (mes
 
     ffmpeg(filePath).format('mp3').save('music.mp3').on('end', async () => {
 
-        form.append('api_token', 'be88a908-3b99-4254-9807-c855d53f6b2b');
+        form.append('api_token', '0e8439a1280a43aba9a5bc0a16f3f009');
 
         form.append('file', fs.createReadStream('./music.mp3'));
 
