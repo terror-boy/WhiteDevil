8650d111d1ca32678cd8697fe84ff0448d420935
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-09 10:08:26 +0530

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -33,19 +33,7 @@ Asena.addCommand({pattern: 'p ?(.*)', fromMe: false, desc: Lang.PLAY_DESC}, (asy
                     });
                 writer.addTag();
 
-                reply = await message.client.sendMessage(message.jid,got.stream(arama[0].image).pipe(fs.createWriteStream(title + '.jpg'));
-        ffmpeg(stream)
-            .audioBitrate(320)
-            .save('./' + title + '.mp3')
-            .on('end', async () => {
-                const writer = new ID3Writer(fs.readFileSync('./' + title + '.mp3'));
-                writer.setFrame('TIT2', arama[0].title)
-                    .setFrame('TPE1', [arama[0].author.name])
-                    .setFrame('APIC', {
-                        type: 3,
-                        data: fs.readFileSync(title + '.jpg'),
-                        description: arama[0].description
-                    ,MessageType.text);
+                reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_PLAY ,MessageType.text);
                 await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.audio, {mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: false});
             });
     }));
