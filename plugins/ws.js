2561e2d530b6990ca3feb721cc0f0bceecd9d720
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-10 18:05:33 +0530

    Add files via upload

---
+++ plugins/ws.js
@@ -0,0 +1,63 @@
+/* Copyright © 2021 TERROR BOY.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+WHITE DEVIL ----»»» TERROR BOY
+*/
+
+const KTB = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('whatsappstatus');
+
+// AUTO VIDEO COMMAND LIST
+
+// THIS PLUGIN IS OWNED BY KARTHIK_TERROR-BOY
+
+
+if (Config.WORKTYPE == 'private') {
+
+    KTB.addCommand({pattern: 'ws', fromMe: true, desc: Lang.WS}, (async (message, match) => {
+
+        var r_text = new Array ();
+
+        r_text[0] = "https://i.imgur.com/WXEksN4.mp4";
+        r_text[1] = "https://imgur.com/3VOuEfg.mp4";
+        r_text[2] = "https://imgur.com/rbGjIBI.mp4";
+        r_text[3] = "https://imgur.com/tt2gMXr.mp4";
+        r_text[4] = "https://imgur.com/kR4XGKY.mp4";
+        
+        
+
+        var i = Math.floor(5*Math.random())
+
+        var respovideo = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+// EXPORTING
+        await message.client.sendMessage(message.jid, Buffer(respovideo.data), MessageType.video, {mimetype: Mimetype.mp4, caption: '*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*'})
+
+    }));
+}
+else if (Config.WORKTYPE == 'public') {
+
+    KTB.addCommand({pattern: 'ws', fromMe: false, desc: Lang.WS}, (async (message, match) => {
+
+        var r_text = new Array ();
+
+        r_text[0] = "https://i.imgur.com/WXEksN4.mp4";
+        r_text[1] = "https://imgur.com/3VOuEfg.mp4";
+        r_text[2] = "https://imgur.com/rbGjIBI.mp4";
+        r_text[3] = "https://imgur.com/tt2gMXr.mp4";
+        r_text[4] = "https://imgur.com/kR4XGKY.mp4";
+        
+        
+
+        var i = Math.floor(5*Math.random())
+
+        var respovideo = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+// EXPORTING 
+        await message.client.sendMessage(message.jid, Buffer(respovideo.data), MessageType.video, {mimetype: Mimetype.mp4, caption: '*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*'})
+
+    }));
+}
