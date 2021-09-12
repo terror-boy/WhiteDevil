e713fd430e5e0f151c8c7bfba87a9b2a69ca87d1
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-12 12:17:49 +0530

    Update warn.js

--- plugins/warn.js
+++ plugins/warn.js
@@ -47,7 +47,7 @@ if (Config.WORKTYPE == 'private') {
     }));
 }
 else if (Config.WORKTYPE == 'public') {
-    Asena.addCommand({pattern: 'warn ?(.*)', fromMe: false, desc: Lang.WREPORT}, (async (message, match) => {
+    Asena.addCommand({pattern: 'warn ?(.*)', fromMe: true, desc: Lang.WREPORT}, (async (message, match) => {
         if (match[1] == '' && message.reply_message) {
             let grup = await message.client.groupMetadata(message.jid);
             var jids = [];
