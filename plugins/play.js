1cb0532ae4ab7f377282fcdf4f279018a9267b03
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-14 09:23:52 +0530

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,40 +1,24 @@
-const Asena = require('../events');
+const Ktb = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 
 const Language = require('../language');
-const Lang = Language.getString('scrapers');
+const Lang = Language.getString('gitlink');
+
+Ktb.addCommand({pattern: 'play', fromMe: false, desc: Lang.GL}, (async (message, match) => {
+
+    var r_text = new Array ();
 
 
-Asena.addCommand({pattern: 'play ?(.*)', fromMe: false, desc: Lang.PLAY_DESC}, (async (message, match) => { 
 
-        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_TEXT_PLAY,MessageType.text);    
-        let arama = await yts(match[1]);
-        arama = arama.all;
-        if(arama.length < 1) return await message.client.sendMessage(message.jid,Lang.NO_RESULT,MessageType.text);
-        var reply = await message.client.sendMessage(message.jid,Lang.DOWNLOADING_PLAY,MessageType.text);
+  r_text[0] = "https://i.imgur.com/kvX50Ch.png";
 
-        let title = arama[0].title.replace(' ', '+');
-        let stream = ytdl(arama[0].videoId, {
-            quality: 'highestaudio',
-        });
-    
-        got.stream(arama[0].image).pipe(fs.createWriteStream(title + '.jpg'));
-        ffmpeg(stream)
-            .audioBitrate(320)
-            .save('./' + title + '.mp3')
-            .on('end', async () => {
-                const writer = new ID3Writer(fs.readFileSync('./' + title + '.mp3'));
-                writer.setFrame('TIT2', arama[0].title)
-                    .setFrame('TPE1', [arama[0].author.name])
-                    .setFrame('APIC', {
-                        type: 3,
-                        data: fs.readFileSync(title + '.jpg'),
-                        description: arama[0].description
-                    });
-                writer.addTag();
 
-                reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_PLAY ,MessageType.text);
-                await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.audio, {mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: false});
-            });
-    }));
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*PLEASE TYPE  [^]musics* \n\n\n\n\n *[^]=handlers like {!,./}* \n\n _CODED BY KARTHIK-TERROR BOY_
+`}) 
+
+}));
