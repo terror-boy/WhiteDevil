ec755c41feb72f6685d296e18b27a40e5825c469
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-16 13:44:01 +0530

    Create broadcastgroups.js

---
+++ plugins/broadcastgroups.js
@@ -0,0 +1,33 @@
+let handler = async (m, { conn, text }) => {
+  let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us')).map(v => v.jid)
+  let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
+  let teks = text ? text : cc.text
+  conn.reply(m.chat, `_Send a broadcast message to ${groups.length} group_\estimate completed ${groups.length * 1.5} second`, m)
+  for (let id of groups) {
+    await delay(1500)
+    await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '〔 Eva Broadcast 〕\n\n' + teks + '\n\n' + watermark), true).catch(_ => _)
+  }
+  m.reply('_*Broadcast Completed*_')
+}
+handler.help = ['broadcastgroup', 'bcgc'].map(v => v + ' <text>')
+handler.tags = ['owner']
+handler.command = /^(broadcast|bc)(group|grup|gc)$/i
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+const randomID = length => require('crypto').randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)
+
+const delay = time => new Promise(res => setTimeout(res, time))
