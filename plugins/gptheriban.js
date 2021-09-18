8958e262456c18811209b268f5acc3c68990fa9a
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-18 14:15:08 +0530

    Update gptheriban.js

--- plugins/gptheriban.js
+++ plugins/gptheriban.js
@@ -3,49 +3,80 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 WHITE DEVIL ----»»» TERROR BOY
 */
-const Ktb = require('../events');
-const config = require('../config');
-const Heroku = require('heroku-client');
-const heroku = new Heroku({
-    token: config.HEROKU.API_KEY
-});
-let baseURI = '/apps/' + config.HEROKU.APP_NAME;
 
-   var l_dsc = ''
-    var alr_on = ''
-    var alr_off = ''
-    var ANTIT_on = ''
-    var ANTIT_off = ''
-   
-    if (config.LANG == 'EN') {
-        l_dsc = 'remove given theri'
-        alr_on = '!'
-        alr_off = '!'
-        ANTIT_off = '*ANTI_THERI TURNED ON*'
-        ANTIT_off = '*ANTI_THERI TURNED OFF*'
-    }
-    if (config.LANG == 'ML') {
-        l_dsc = '.'
-        alr_on = '!'
-        alr_off = '!'
-        ANTIT_on = 'ANTI_THERI TURNED ON'
-        ANTIT_off = 'ANTI_THERI TURNED Off'
-    }
-   
-    Ktb.addCommand({pattern: 'whitetheri ?(.*)', fromMe: true, desc: l_dsc, usage: '.whitetheri on / of' }, (async (message, match) => {
-        if (match[1] == 'off') {
-                await heroku.patch(baseURI + '/config-vars', { 
-                    body: { 
-                        ['ANTI_THERI']: 'false'
-                    } 
-                });
-                await message.sendMessage(ANTIT_off)
-        } else if (match[1] == 'on') {
-                await heroku.patch(baseURI + '/config-vars', { 
-                    body: { 
-                        ['ANTI_THERI']: 'true'
-                    } 
-                });
-                await message.sendMessage(ANTIT_on)
-        }
-    }));
+const Hisham = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const exec = require('child_process').exec;
+const os = require("os");
+const fs = require('fs');
+const Config = require('../config')
+
+
+const ANTI_THERI = "➶➶➶➶➶ 𝙒𝙃𝙄𝙏𝙀𝘿𝙀𝙑𝙄𝙇 ➷➷➷➷➷ \n\n\n *·÷±‡±𝐌𝐎𝐍𝐄 𝐈𝐓𝐇 𝐈𝐕𝐈𝐃𝐄 𝐕𝐄𝐍𝐃𝐀.𝐆𝐎𝐎𝐃 𝐁𝐘𝐄🖐🏻🖐±‡±÷·* \n\n *—(••÷[ മോനേ ഇത് ഇവിടെ വേണ്ട.𝔾𝕆𝕆𝔻 𝔹𝕐𝔼🖐🏻🖐🏻 ]÷••)—*"
+ if (Config.ANTI_THERI == 'true') {
+Hisham.addCommand({on: 'text', fromMe: false, delownsewcmd: false, onlyPm: false }, (async (message, match) => {
+        let regexb1ichu = new RegExp('thayoli')
+        let regexb2ichu = new RegExp('myre')
+        let regexb3ichu = new RegExp('kunna')
+        let regexb4ichu = new RegExp('poorimone')
+        let regexb5ichu = new RegExp('myr')
+        let regexb6ichu = new RegExp('patti')
+        let regexb7ichu = new RegExp('oombi')
+        let regexb8ichu = new RegExp('thendi')
+        let regexb9ichu = new RegExp('thendi')
+// export data -(Hisham-muhammed)
+          if (regexb1ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + ANTI_THERI + '*', MessageType.text);
+             await message.client.groupRemove(message.jid, [message.data.participant]);
+          } 
+        else if (regexb2ichu.test(message.message)) {
+          
+           await message.client.sendMessage(message.jid, '*' + ANTI_THERI + '*', MessageType.text);
+             await message.client.groupRemove(message.jid, [message.data.participant]);
+          }
+         else if (regexb3ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + ANTI_THERI + '*', MessageType.text);
+             await message.client.groupRemove(message.jid, [message.data.participant]);
+          }
+        else if (regexb4ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + ANTI_THERI + '*', MessageType.text);
+             await message.client.groupRemove(message.jid, [message.data.participant]);
+          }
+          else if (regexb5ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + ANTI_THERI + '*', MessageType.text);
+             await message.client.groupRemove(message.jid, [message.data.participant]);
+          }
+          else if (regexb6ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + ANTI_THERI + '*', MessageType.text);
+             await message.client.groupRemove(message.jid, [message.data.participant]);
+          }
+         else if (regexb6ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + ANTI_THERI + '*', MessageType.text);
+             await message.client.groupRemove(message.jid, [message.data.participant]);
+          }
+         else if (regexb7ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + ANTI_THERI + '*', MessageType.text);
+             await message.client.groupRemove(message.jid, [message.data.participant]);
+          }
+         else if (regexb8ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + ANTI_THERI + '*', MessageType.text);
+             await message.client.groupRemove(message.jid, [message.data.participant]);
+          }
+         else if (regexb9ichu.test(message.message)) {
+           
+            await message.client.sendMessage(message.jid, '*' + ANTI_THERI + '*', MessageType.text);
+             await message.client.groupRemove(message.jid, [message.data.participant]);
+          }
+          
+}));
+
+
+}
