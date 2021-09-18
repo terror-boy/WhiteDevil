1c35a67d85b81e221665436c8d12cb6cae5acef5
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-18 11:54:15 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -33,5 +33,5 @@ Asena.addCommand({pattern: 'fb ?(.*)', fromMe: wk, desc: CLR_DESC, usage: 'fb ht
     auth_message.share + payload.share + '\n' +
     auth_message.comment + payload.comment + '\n' +
     auth_message.creation + payload.created_at
-  await message.sendMessage(Buffer.from(video.data), MessageType.video, { caption: made by WhiteDevil, mimetype: Mimetype.mp4 })
+  await message.sendMessage(Buffer.from(video.data), MessageType.video, { caption: 'made by WhiteDevil', mimetype: Mimetype.mp4 })
 }));
