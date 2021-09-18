c0843157ac08091bcd6e1eb6bad3bd6b0b969044
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-18 14:26:36 +0530

    Create whitedeviltheri.js

---
+++ plugins/whitedeviltheri.js
@@ -0,0 +1,42 @@
+const Asena = require('../events');
+const config = require('../config');
+const Heroku = require('heroku-client');
+const heroku = new Heroku({
+    token: config.HEROKU.API_KEY
+});
+let baseURI = '/apps/' + config.HEROKU.APP_NAME;
+
+
+ var AT = ''
+ var T = ''
+
+  if (config.LANG == 'EN') {
+
+     AT = 'ᴀɴᴛɪ ᴛʜᴇʀɪ ᴏɴ'
+     T = 'ᴀɴᴛɪ ᴛʜᴇʀɪ ᴏꜰꜰ'
+
+    }
+
+    if (config.LANG == 'ML') {
+
+     AT = 'ᴀɴᴛɪ ᴛʜᴇʀɪ ᴏɴ'
+     T = 'ᴀɴᴛɪ ᴛʜᴇʀɪ ᴏꜰꜰ'
+    }
+
+ Asena.addCommand({pattern: 'wtheri ?(.*)', fromMe: true,dontAddCommandList: true,desc: 'change theri on/off. example - .wtheri off/on' }, (async (message, match) => {
+        if (match[1] == 'on') {
+                await heroku.patch(baseURI + '/config-vars', { 
+                    body: { 
+                        ['ANTI_THERI']: 'true'
+                    } 
+                });
+                await message.sendMessage(AT)
+        } else if (match[1] == 'off') {
+                await heroku.patch(baseURI + '/config-vars', { 
+                    body: { 
+                        ['ANTI_THERI']: 'false'
+                    } 
+                });
+                await message.sendMessage(T)
+        }
+    }));
