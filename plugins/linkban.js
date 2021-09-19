a65738268ca6bb15c1b9c2b9ddce563a225b91dd
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-19 21:46:38 +0530

    Create linkban.js

---
+++ plugins/linkban.js
@@ -0,0 +1,76 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const exec = require('child_process').exec;
+const os = require("os");
+const fs = require('fs');
+const Config = require('../config')
+const Language = require('../language');
+const Lang = Language.getString('evaluators');
+const SLang = Language.getString('conventer');
+const NLang = Language.getString('scrapers');
+const googleTTS = require('google-translate-tts');
+const Heroku = require('heroku-client');
+const heroku = new Heroku({
+    token: Config.HEROKU.API_KEY
+});
+
+
+let baseURI = '/apps/' + Config.HEROKU.APP_NAME;
+
+
+async function checkUsAdmin(message, user = message.data.participant) {
+    var grup = await message.client.groupMetadata(message.jid);
+    var sonuc = grup['participants'].map((member) => {     
+        if (member.jid.split("@")[0] == user.split("@")[0] && member.isAdmin) return true; else; return false;
+    });
+    return sonuc.includes(true);
+}
+async function checkImAdmin(message, user = message.client.user.jid) {
+    var grup = await message.client.groupMetadata(message.jid);
+    var sonuc = grup['participants'].map((member) => {     
+        if (member.jid.split("@")[0] == user.split("@")[0] && member.isAdmin) return true; else; return false;
+    });
+    return sonuc.includes(true);
+
+}
+var alllink_var = ''
+async function megalink() {
+    await heroku.get(baseURI + '/config-vars').then(async (vars) => {
+        alllink_var = vars.ALL_LINK_BAN
+    });
+}
+megalink()
+var ldc = ''
+if (Config.LANG == 'AZ') ldc = '*Bağlantı Aşkarlandı!*'
+if (Config.LANG == 'TR') ldc = '*‎Link Tespit Edildi!*'
+if (Config.LANG == 'EN') ldc = '➶➶➶➶➶ 𝙒𝙃𝙄𝙏𝙀𝘿𝙀𝙑𝙄𝙇 ➷➷➷➷➷ \n\n\n *ᴡʜᴀᴛꜱᴀᴘᴘ ʟɪɴᴋ ᴅᴇᴛᴇᴄᴛᴇᴅ! ᴍᴏɴᴇ ɪᴛʜ ɪᴠɪᴅᴇ ᴀʟʟᴏᴡᴇᴅ ᴇʟʟᴀ... , ᴏᴋᴇ ʙʏᴇ* \n\n *🕊🕊*'
+if (Config.LANG == 'ML') ldc = '➶➶➶➶➶ 𝙒𝙃𝙄𝙏𝙀𝘿𝙀𝙑𝙄𝙇 ➷➷➷➷➷ \n\n\n *ᴡʜᴀᴛꜱᴀᴘᴘ ʟɪɴᴋ ᴅᴇᴛᴇᴄᴛᴇᴅ! ᴍᴏɴᴇ ɪᴛʜ ɪᴠɪᴅᴇ ᴀʟʟᴏᴡᴇᴅ ᴇʟʟᴀ... , ᴏᴋᴇ ʙʏᴇ* \n\n *🕊🕊*'
+if (Config.LANG == 'ID') ldc = '*Tautan Terdeteksi!*'
+if (Config.LANG == 'PT') ldc = '*Link Detectado!*'
+if (Config.LANG == 'RU') ldc = '*Ссылка обнаружена!*'
+if (Config.LANG == 'HI') ldc = '*लिंक का पता चला!*'
+if (Config.LANG == 'ES') ldc = '*Enlace Detectado!*'
+Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (message, match) => {
+    if (alllink_var == 'true' && message.jid !== '9082891698-1616356915@g.us') {
+       let regex1 = new RegExp('https://chat.whatsapp.com/')
+       let regex2 = new RegExp('chat.whatsapp.com/')
+
+        if (regex1.test(message.message)) {
+            var us = await checkUsAdmin(message)
+            var im = await checkImAdmin(message)
+            if (!im) return;
+            if (us) return;
+            await message.client.groupRemove(message.jid, [message.data.participant]);         
+            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
+        } 
+        else if (regex2.test(message.message)) {
+            var us = await checkUsAdmin(message)
+            var im = await checkImAdmin(message)
+            if (!im) return;
+            if (us) return;
+            await message.client.groupRemove(message.jid, [message.data.participant]);         
+            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
+        
+      }
+    }
+}));
