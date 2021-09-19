784f64576638bc578ff87558f8a3f8b585778117
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-19 21:25:36 +0530

    Update groupclose.js

--- plugins/groupclose.js
+++ plugins/groupclose.js
@@ -15,9 +15,9 @@ const CL = ('This command for Close the  Group at night');
 const code = ('!mute 10h')
 const FINAL = "THIS GROUP IS CLOSED & OPEN IN MORNING"
 const TMUTE = "Trying to close temporary,\nAttempting to temporarily close the group"
-const TST = "!tagall !gn"
+const TST = "!tag !gn"
 const SCXR = "Running script...."
-Asena.addCommand({pattern: 'close', fromMe: true, deleteCommand: false, desc: CL,}, (async (message, match) => {
+Asena.addCommand({pattern: 'close', fromMe: true, deleteCommand: true, desc: CL,}, (async (message, match) => {
 
   var msg = await message.reply('Preforming....');
 
