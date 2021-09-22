11d79b2a1650f2acda9595ddaf7210dc6d9b0031
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-22 13:46:29 +0530

    Update conventer.js

--- plugins/conventer.js
+++ plugins/conventer.js
@@ -166,7 +166,7 @@ else if (Config.WORKTYPE == 'public') {
             .fromFormat('webp_pipe')
             .save('output.jpg')
             .on('end', async () => {
-                await message.client.sendMessage(mid, fs.readFileSync('output.jpg'), MessageType.image, quoted: message.data, {mimetype: Mimetype.jpg});
+                await message.client.sendMessage(mid, fs.readFileSync('output.jpg'), MessageType.image, {mimetype: Mimetype.jpg, quoted: message.data});
             });
         return await message.client.deleteMessage(mid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
     }));
