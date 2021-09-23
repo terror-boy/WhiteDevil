2a1deeafb6eed8089e10cca1d36789bf64267512
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-23 13:40:30 +0530

    Update removebg.js

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -52,7 +52,7 @@ if (Config.WORKTYPE == 'private') {
 		    fs.createWriteStream('rbg.png')
         );
     
-        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'White Devil.png', mimetype: Mimetype.png});
+        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'White Devil.png', mimetype: Mimetype.png , quoted: message.data});
         await load.delete();
     }));
 }
@@ -88,7 +88,7 @@ else if (Config.WORKTYPE == 'public') {
 		    fs.createWriteStream('rbg.png')
         );
     
-        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'White Devil.png', mimetype: Mimetype.png});
+        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'White Devil.png', mimetype: Mimetype.png , quoted: message.data});
         await load.delete();
     }));
     
