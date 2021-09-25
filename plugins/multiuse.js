f6f35ac2741a7a551850ec6ae409fe55c1680ca7
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-25 22:04:30 +0530

    Update multiuse.js

--- plugins/multiuse.js
+++ plugins/multiuse.js
@@ -6,17 +6,18 @@ const UPLOAD_ING = "*✅️Connected To INSTAGRAM* \n\n\n *DOWNLOADING & UPLOADI
 
 Ktb.addCommand({pattern: 'insta ?(.*)', fromMe: true, desc: "Downloads from instagaram" , dontAddCommandList: true }, async (message, match) => {
     
-    const {data} = await axios(`https://zenzapi.xyz/api/downloader/instagram?url=${match[1]}&apikey=farhandqz`)
+    const {data} = await axios(`https://zenzapi.xyz/api/downloader/instagram?url=${match[1]}&apikey=whitedevil-terrorboy`)
     const { status, result } = data
 if(!status) return await message.sendMessage('not found')
 var reply = await message.client.sendMessage(message.jid, LOAD_ING ,MessageType.text);
 
-let julie =  await axios.get(`${result.url}`)
+let  =  await axios.get(`${result.url}`)
 const msg = `${result.type}`
  reply = await message.client.sendMessage(message.jid,UPLOAD_ING ,MessageType.text);
 
-  if (type === 'image') { await message.sendMessage(data, MessageType.image,{ caption: "*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*" }) }
-    else if (type === 'video') { await message.sendMessage(data, MessageType.video,{ caption: "*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*" }) }
+  if ktb (type === 'image') { await message.sendMessage(Buffer.from(ktb.data), MessageType.image,{ caption: "*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*" }) }
+
+    else if (type === 'video') { await message.sendMessage(Buffer.from(ktb.data), MessageType.video,{ caption: "*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*" }) }
     
  return await message.client.sendMessage(message.jid, "```Link Invalid```", MessageType.text);
 });
