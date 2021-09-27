fa887422ce133f9736c03b811248748debf6b19f
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-27 22:22:18 +0530

    Add files via upload

--- plugins/contact.js
+++ plugins/contact.js
@@ -1,15 +1,31 @@
-let handler = async (m, { conn, text }) => {
-  if (!text) return
-  let who
-  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-  else who = m.chat
-  if (!who) throw 'Tag one'
-  txt = text.replace('@' + who.split`@`[0], '').trimStart()
-  return conn.sendContact(m.chat, who, txt || conn.getName(who), m)
-}
-handler.help = ['save'].map(v => v + ' @mention <contact name>')
-handler.tags = ['tools']
+/* Copyright (C) 2021 TERROR BOY.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+WHITE DEVIL ----»»» TERROR BOY
+*/
 
-handler.command = /^save$/
+const Ktb = require('../events');
+const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const Config = require('../config')
+const axios = require('axios')
+const request = require('request');
+const os = require('os');
+var clh = { cd: 'L3Jvb3QvV2hhdHNBc2VuYUR1cGxpY2F0ZWQv', pay: '' }    
+var ggg = Buffer.from(clh.cd, 'base64')
+var ddd = ggg.toString('utf-8')
 
-module.exports = handler
+Ktb.addCommand({pattern: 'ownerr', fromMe: false, dontAddCommandList: false}, (async (message, match) => {
+// send a sendContact!
+    const sendContact = [
+      '918086169178', karthik 
+      ]
+
+
+      
+
+      }
+      
+      await message.client.sendMessage(message.jid, '0@s.whatsapp.net', MessageType.buttonsMessage)
+
+}));
