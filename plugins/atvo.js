6c1ec3679fb73e9c6d0214ee26f52a7f4b3e4578
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-30 20:27:46 +0530

    Update atvo.js

--- plugins/atvo.js
+++ plugins/atvo.js
@@ -19,9 +19,9 @@ var ddd = ggg.toString('utf-8')
  Ktb.addCommand({pattern: 'avdo', fromMe:false , desc: Lang.AV}, (async (message, match) => {
 
     
-      const autovideoMessage = {
+      const autovideoMessage = [
           "𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 Karthik_terror-boy's 𝐛𝐨𝐭.\n\n*WhiteDevil*\n\n📽 *AUTO VIDEO* 📽\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *Free fire*\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *ff*\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *Manchester United*\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *Manchester*\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *Manju warrier*\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *Manju* \n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *Whitedevil*\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *pubg*\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *life*\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*\n\n *[IF DID NOT GET RESULT IT MAY BE BOT IS UNDER MAINTANCE OR ELSE IF YOU USE HANDLERS LIKE(!/,.#)]*\n\n═════💢WhiteDevil💢═════\n\n\n▣▣▣▣▣▣▣▣▣ OWNER: Karthik_terror-boy  ▣▣▣▣▣▣▣▣▣▣▣"
-      };
+      ]
       
       await message.client.sendMessage(message.jid, autovideoMessage , MessageType.text,)
 
