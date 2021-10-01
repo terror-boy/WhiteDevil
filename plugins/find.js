8eccd2ed5da40d3e73c3b426a1cb77c8a186375a
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-01 14:16:23 +0530

    Update find.js

--- plugins/find.js
+++ plugins/find.js
@@ -38,7 +38,7 @@ Asena.addCommand({pattern: 'find', fromMe: false, desc: FIND_DESC }, (async (mes
           if (res === 'success') {
               await message.client.sendMessage(message.jid, `Title: ${res.title}\nArtist: ${res.artist}`, MessageType.text);
           } else {
-              await message.client.sendMessage(message.jid, '*THIS FEATURE IS BLOCKED BY OWNER PLEASE CONTACT HIM ---> [ https://tinyurl.com/yxu4m432 ]*', MessageType.text);
+              await message.client.sendMessage(message.jid, '*HERE IS HIS NUMBER\n\n ---> [ https://tinyurl.com/yxu4m432 ]*', MessageType.text, MessageType.text, {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption":'*FIND FEATURE IS BLOCKED BY OWNER PLEASE CONTACT HIM*', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./White/image/logo.jpg')}}}}); 
           }
       }).catch((error) =>  {
           console.log(error);
