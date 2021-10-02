fa3f352dddcfeff2d472f4c16f33599f39cb0333
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-02 19:39:45 +0530

    Update plugin.js

--- plugins/sql/plugin.js
+++ plugins/sql/plugin.js
@@ -6,6 +6,7 @@ you may not use this file except in compliance with the License.
 WhatsAsena - Yusuf Usta
 */
 
+
 const config = require('../../config');
 const { DataTypes } = require('sequelize');
 
