4c538f290d560140337130017ba60224a5fa9df2
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-02 15:47:36 +0530

    Create oo.js

---
+++ plugins/oo.js
@@ -0,0 +1,56 @@
+/* Copyright © 2021 TERROR BOY.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+WHITE DEVIL ----»»» TERROR BOY
+*/
+
+const Ktb = require('../events');
+const {MessageType,Mimetype} = require('@adiwajshing/baileys');
+const translatte = require('translatte');
+const Language = require('../language');
+const Config = require('../config');
+const axios = require('axios')
+
+let TB = Config.WORKTYPE == 'public' ? false : true
+
+Ktb.applyCMD({ pattern: 'splay ?(.*)', fromMe: TB,  deleteCommand: false, desc: Lang.SONG_DESC}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_TEXT_SONG,MessageType.text, {quoted: message.data});   
+    let arama = await yts(match[1]);
+    arama = arama.all;
+    if(arama.length < 1) return await message.client.sendMessage(message.jid,Lang.NO_RESULT,MessageType.text, {quoted: message.data});
+    var downloading = await message.client.sendMessage(message.jid,config.SONGD,MessageType.text, {quoted: message.data});
+  
+    let title = arama[0].title.replace(' ', '+');
+    let stream = ytdl(arama[0].videoId, {
+        quality: 'highestaudio',
+    });
+    
+    got.stream(arama[0].image).pipe(fs.createWriteStream(title + '.jpg'));
+    ffmpeg(stream)
+        .audioBitrate(320)
+        .save('./' + title + '.mp3')
+        .on('end', async () => {
+            const writer = new ID3Writer(fs.readFileSync('./' + title + '.mp3'));
+            writer.setFrame('TIT2', arama[0].title)
+                .setFrame('TPE1', [arama[0].author.name])
+                .setFrame('APIC', {
+                    type: 3,
+                    data: fs.readFileSync(title + '.jpg'),
+                        description: arama[0].description
+                });
+            writer.addTag();
+            
+                var uploading = await message.client.sendMessage(message.jid,Config.SONGU,MessageType.text, {quoted: message.data});
+                await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true});
+                await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.document, {quoted: message.data, filename: title + '.mp3', mimetype: 'audio/mpeg'});
+                return await message.client.deleteMessage(message.jid, {id: uploading.key.id, remoteJid: message.jid, fromMe: true})
+            }
+
+                var uploading = await message.client.sendMessage(message.jid,Config.SONGU,MessageType.text, {quoted: message.data});
+                await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true});
+                await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: false});
+                return await message.client.deleteMessage(message.jid, {id: uploading.key.id, remoteJid: message.jid, fromMe: true})
+            }
+        });
+}));
