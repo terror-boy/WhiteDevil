652402ac55c64f11b5b68cd61fdfc7db9013ff33
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-04 10:21:38 +0530

    Update istory.js

--- plugins/istory.js
+++ plugins/istory.js
@@ -12,21 +12,21 @@ const LOAD_ING = "*✅️Connecting To INSTAGRAM*"
 const UPLOAD_ING = "*✅️Connected To INSTAGRAM* \n\n\n *DOWNLOADING STORY & UPLOADING IN PROCESS...*"
 
 Ktb.addCommand({pattern: 'story ?(.*)', fromMe: false, desc: "Downloads STORY from instagaram" , dontAddCommandList: true }, async (message, match) => {
-    
-    var reply = await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
     if (match[0].includes('install')) return;
         if (match[1] === '') return await message.client.sendMessage(message.jid,'*need username*', MessageType.text, { quoted: message.data });
-  const {data} = await axios(`https://xteam.xyz/dl/igs?nama=${match[1]}&APIKEY=ab9942f95c09ca89`)
-    const { status, result } = data
-     if(!status) return await message.sendMessage('api error')
-         let ktb =  await axios.got(`${result.url}`)
-         const msg = `${result.type}`
-
-    reply = await message.client.sendMessage(message.jid,UPLOAD_ING , MessageType.text, { quoted: message.data });
+    var reply = await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
     
-    if (type === 'image') return await message.sendMessage(Buffer.from(ktb.data), MessageType.image, { caption: "*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*", quoted: message.data })
     
-    if (type === 'video') return await message.sendMessage(Buffer.from(ktb.data), MessageType.video, { caption: "*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*", quoted: message.data })
+  const data = await axios(`https://xteam.xyz/dl/igs?nama=${match[1]}&APIKEY=ab9942f95c09ca89`)
+    try {
+		const response = await got(data);
+       const msg = `${data.type}`
+       
+    if (response.statusCode === 200) return await message.client.sendMessage(message.jid,UPLOAD_ING , MessageType.text, { quoted: message.data });
+    
+    if (type === 'image') return await message.sendMessage(Buffer.from(msg.data), MessageType.image, { caption: "*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*", quoted: message.data })
+    
+    if (type === 'video') return await message.sendMessage(Buffer.from(msg.data), MessageType.video, { caption: "*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*", quoted: message.data })
     
 });
     
