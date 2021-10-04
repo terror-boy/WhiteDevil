815070051f5ce0e67ce3ffa22a60e3335fb2e8c1
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-04 13:13:24 +0530

    Create maker.js

---
+++ plugins/maker.js
@@ -0,0 +1,43 @@
+/* Copyright © 2021 TERROR BOY.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+WHITE DEVIL ----»»» TERROR BOY
+*/
+
+const Ktb = require('../events');
+const {WAConnection, MessageOptions, MessageType, Mimetype, Presence} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const axios = require('axios');
+
+
+Ktb.addCommand({pattern: 'glue ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+    if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD);
+
+    var webimage = await axios.get(`https://zenzapi.xyz/api/textpro/gluetext?text=${match[1]}&apikey=whitedevil-terrorboy`, { responseType: 'arraybuffer' })
+
+  await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg , caption: '*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*'})
+
+}));
+
+Ktb.addCommand({pattern: 'wasted ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+  
+    if (match[1] === '') return await message.client.sendMessage(message.jid,"need image url");
+
+    var webimage = await axios.get(`https://zenzapi.xyz/api/image/wasted?image=${match[1]}&apikey=whitedevil-terrorboy`, { responseType: 'arraybuffer' })
+
+  await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg , caption: '*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*'})
+
+}));
+
+Ktb.addCommand({pattern: 'delete ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+
+  
+    if (match[1] === '') return await message.client.sendMessage(message.jid,"need image url");
+
+    var webimage = await axios.get(`https://zenzapi.xyz/api/image/delete?image=${match[1]}&apikey=whitedevil-terrorboy`, { responseType: 'arraybuffer' })
+
+  await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg , caption: '*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*'})
+
+}));
