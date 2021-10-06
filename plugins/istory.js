55f8ebcfb908d8bf4696e49e5592146a50c4b014
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-06 14:55:48 +0530

    Update istory.js

--- plugins/istory.js
+++ plugins/istory.js
@@ -16,3 +16,24 @@ Ktb.addCommand({pattern: 'story ?(.*)', fromMe: false, desc: "Downloads STORY fr
         if (match[1] === '') return await message.client.sendMessage(message.jid,'*need username*', MessageType.text, { quoted: message.data });
     var reply = await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
 
+
+  const data = await axios(`https://xteam.xyz/dl/igs?nama=${match[1]}&APIKEY=ab9942f95c09ca89`)
+    try {
+		const response = await got(data);
+       const msg = `${data.type}`
+
+    if (response.statusCode === 200) return await message.client.sendMessage(message.jid,UPLOAD_ING , MessageType.text, { quoted: message.data });
+
+    } catch {
+
+    if (type === 'image') return await message.sendMessage(Buffer.from(msg.data), MessageType.image, { caption: "*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*", quoted: message.data })
+
+    if (type === 'video') return await message.sendMessage(Buffer.from(msg.data), MessageType.video, { caption: "*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*", quoted: message.data })
+
+    }
+});
+
+
+
+
+
