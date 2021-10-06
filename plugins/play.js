3cdd7391f770974f82c15f35b9a58d551fd5bdab
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-06 18:49:20 +0530

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -7,17 +7,16 @@ const Config = require('../config');
 Asena.addCommand({pattern: 'play ?(.*)', fromMe: false, desc: 'play song' , dontAddCommandList: true }, async (message, match) => {
 	if (match[0].includes('install')) return;
 	
-        if (match[1] === '') return await message.client.sendMessage(message.jid,'*need song name*', MessageType.text, { quoted: message.data });
+        if (match[1] === '') return await message.client.sendMessage(message.jid,'need song name', MessageType.text, { quoted: message.data });
 	
 	const url = `https://zenzapi.xyz/api/play/playmp3?query=${match[1]}&apikey=whitedevil-terrorboy`;
 	try {
-		const response = await got(url);
+		const response = await axios.get (json.url);
     
 		const json = JSON.parse(response.body);
     
-		if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '``` THUMBNAIL      : *' + json.thumb + '* \n' + 'TITLE      : *' + json.title + '* \n' + 'CHANNEL      :'+ json.channel + 'DATE OF PUBLISHED    :' + json.published + '\n' + 'TOTAL VIEWS   :'+ json.views + '\n' +  'URL  :'+ json.url + '```' , MessageType.text);
+		if (response.statusCode === 200) return await axios.get (json.url)
       } catch {
 		return await message.client.sendMessage(message.jid,'NOT FOUND', MessageType.text);
 	}
 });
-
