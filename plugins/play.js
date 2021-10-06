c2108df60a0ee416ca099d2bd783e8f5f0793340
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-06 18:01:41 +0530

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -5,6 +5,10 @@ const Config = require('../config');
 
 
 Asena.addCommand({pattern: 'play ?(.*)', fromMe: false, desc: 'play song' , dontAddCommandList: true }, async (message, match) => {
+	if (match[0].includes('install')) return;
+	
+        if (match[1] === '') return await message.client.sendMessage(message.jid,'*need song name*', MessageType.text, { quoted: message.data });
+	
 	const url = `https://zenzapi.xyz/api/play/playmp3?query=${match[1]}&apikey=whitedevil-terrorboy`;
 	try {
 		const response = await got(url);
