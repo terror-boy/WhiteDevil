d78ea346f20a95e7e8328b8da1e4b412269f1adf
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-13 15:10:11 +0530

    Create matrix.js

---
+++ plugins/matrix.js
@@ -0,0 +1,35 @@
+const thiccysapi = require('textmaker-thiccy'); // Import NPM Package
+
+const Asena = require('../events');
+const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const Config = require('../config')
+const axios = require('axios')
+const request = require('request');
+const os = require('os');
+var clh = { cd: 'L3Jvb3QvV2hhdHNBc2VuYUR1cGxpY2F0ZWQv', pay: '' }    
+var ggg = Buffer.from(clh.cd, 'base64')
+var ddd = ggg.toString('utf-8')
+clh.pay = ddd
+
+let wk = Config.WORKTYPE == 'public' ? false : true
+
+Asena.addCommand({pattern: 'matrix ?(.*)', fromMe: wk, dontAddCommandList: true}, (async (message, match) => {
+    thiccysapi.textpro("https://textpro.me/matrix-style-text-effect-online-884.html",
+        `${match[1]}`
+        ).then(async (data) => { 
+          try { 
+              var download = async(uri, filename, callback) => {
+                  await request.head(uri, async(err, res, body) => {    
+                      await request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+                  });
+              };
+
+              await download(`${data}`, '/root/WhatsAsenaDuplicated/devil.jpg', async() => {                          
+                  await message.client.sendMessage(message.jid,fs.readFileSync('/root/WhatsAsenaDuplicated/devil.jpg'), MessageType.image, { caption: '*WHITEDEVIL*' })
+              })
+          } catch(err) { 
+              console.log(err)
+          } 
+    });
+}));
