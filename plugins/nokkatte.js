e61cb9f636c0c1813ae73f2262618aaf5a23f6c2
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-16 07:38:11 +0530

    Update nokkatte.js

--- plugins/nokkatte.js
+++ plugins/nokkatte.js
@@ -1,54 +1,18 @@
-/* const Ktb = require('../events');
+const Ktb = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 const Config = require('../config');
 
 Ktb.addCommand({ pattern: 'ktb ?(.*)', fromMe: false, desc: b64, usage: usage }, async (message, match) => {
 
-const id = '918096169178@s.whatsapp.net' // the WhatsApp ID 
-// send a simple text!
-const sentMsg  = await conn.sendMessage (id, 'oh hello there', MessageType.text)
-// send a location!
-const sentMsg  = await conn.sendMessage(id, {degreesLatitude: 24.121231, degreesLongitude: 55.1121221}, MessageType.location)
+
 // send a contact!
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Jeff Singh\n' // full name
-            + 'ORG:Ashoka Uni;\n' // the organization of the contact
-            + 'TEL;type=CELL;type=VOICE;waid=911234567890:+91 12345 67890\n' // WhatsApp ID + phone number
+            + 'FN:KARTHIK \n' 
+            + 'ORG:WHITEDEVIL;\n'
+            + 'TEL;type=CELL;type=VOICE;waid=919778042644:+91 97780 42644\n' 
             + 'END:VCARD'
-const sentMsg  = await conn.sendMessage(id, {displayname: "Jeff", vcard: vcard}, MessageType.contact)
-// send a list message!
-const rows = [
- {title: 'Row 1', description: "Hello it's description 1", rowId:"rowid1"},
- {title: 'Row 2', description: "Hello it's description 2", rowId:"rowid2"}
-]
-
-const sections = [{title: "Section 1", rows: rows}]
-
-const button = {
- buttonText: 'Click Me!',
- description: "Hello it's list message",
- sections: sections,
- listType: 1
-}
-
-const sendMsg = await conn.sendMessage(id, button, MessageType.listMessage)
-
-// send a buttons message!
-const buttons = [
-  {buttonId: 'id1', buttonText: {displayText: 'Button 1'}, type: 1},
-  {buttonId: 'id2', buttonText: {displayText: 'Button 2'}, type: 1}
-]
-
-const buttonMessage = {
-    contentText: "Hi it's button message",
-    footerText: 'Hello World',
-    buttons: buttons,
-    headerType: 1
-}
-
-const sendMsg = await conn.sendMessage(id, buttonMessage, MessageType.buttonsMessage)
+const sentMsg  = await conn.sendMessage(id, {displayname: "WHITEDEVIL", vcard: vcard}, MessageType.contact)
 
 }));
-*/
