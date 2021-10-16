eebb460afb6d81df52fe9b02d7cf077b279f1e56
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-16 16:39:35 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -9,6 +9,6 @@ Asena.addCommand({pattern: 'owner', fromMe: false, desc: "OWNER NUMBER"}, (async
 
     
 
-const ownr = "*BUT DON'T SPAM IN HIS PM OR DISTURB BY CALLING.*\n*IF HE DOESN'T REPLY WAIT FOR ONE DAY HE WILL REPLY.*"
-await message.client.sendMessage(message.jid, ownr, MessageType.text , {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": '*YOU CAN CONTACT MY OWNER,HE WILL HELP YOU.*', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./White/image/logo.jpg')}}}});
+const ownr = "*YOU CAN CONTACT MY OWNER,HE WILL HELP YOU.* \n\n *BUT DON'T SPAM IN HIS PM OR DISTURB BY CALLING.*\n*IF HE DOESN'T REPLY WAIT FOR ONE DAY HE WILL REPLY.*"
+await message.client.sendMessage(message.jid, ownr, MessageType.text , { quoted: message.data})
 }))
