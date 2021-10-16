36ae8bd019bdc6d1eae7b2e489ed4de73038693e
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-16 13:45:06 +0530

    Update nokkatte.js

--- plugins/nokkatte.js
+++ plugins/nokkatte.js
@@ -4,14 +4,14 @@ const exec = require('child_process').exec;
 const os = require("os");
 const fs = require('fs');
 const Config = require('../config')
-Ktb.addCommand({ pattern: 'ktb ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
+Ktb.addCommand({ pattern: 'owner ?(.*)', fromMe: false, desc: 'owner number' }, (async (message, match) => {
 
 //coded by saidali
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:karthik\n' // full name
-            + 'ORG:whitedevil;\n' // 
-            + 'TEL;type=CELL;type=VOICE;waid=919778042644:+91 9778042644\n' // WhatsApp ID + phone number
+            + 'FN:KARTHIK [WHITEDEVIL]\n' // full name
+            + 'ORG:WhiteDevil;\n' // 
+            + 'TEL;type=CELL;type=VOICE;waid=994403216454:+91 9778042644\n' // WhatsApp ID + phone number
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "whitedevil", vcard: vcard}, MessageType.contact)
+await message.client.sendMessage(message.jid,{displayname: "KARTHIK [WHITEDEVIL]", vcard: vcard}, MessageType.contact)
 }))
