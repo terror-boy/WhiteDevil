14ed7852a27ff3e0319d5a6882d07a2fa97736c6
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-16 13:45:19 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -5,23 +5,10 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'owner', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'owner', fromMe: false, desc: "OWNER NUMBER"}, (async (message, match) => {
 
-    var r_text = new Array ();
     
-    
-   
-  r_text[0] = "https://avatars.githubusercontent.com/u/85664936?v=4";
-    
-    
-    var i = Math.floor(1*Math.random())
 
-    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
-
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: ` *ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴋᴀʀᴛʜɪᴋ_ᴛᴇʀʀᴏʀ_ʙᴏʏ*
-*owner number wa.me/919778042644*
-
- *ABOUT ME          _https://github.com/terror-boy_*
-`}) 
-
-}));
+const ownr = "YOU CAN CONTACT MY OWNER,HE WILL HELP YOU.BUT DON'T SPAM IN HIS PM OR DISTURB BY CALLING.IF HE DOESN'T REPLY WAIT FOR ONE DAY HE WILL REPLY."
+await message.client.sendMessage(message.jid, ownr}, MessageType.text)
+}))
