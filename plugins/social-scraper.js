fb78acccb8b09c209702bc25a13f2231975f27a2
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-17 17:59:28 +0530

    Update social-scraper.js

--- plugins/social-scraper.js
+++ plugins/social-scraper.js
@@ -93,7 +93,7 @@ else if (cn.WORKTYPE == 'public') {
         let url = json.result.data[0].data;
         let name = json.result.data[0].type;
         await axios({ method: "get", url, headers: { 'DNT': 1, 'Upgrade-Insecure-Request': 1 }, responseType: 'arraybuffer'}).then(async (res) => {
-            if (name === 'video') { return await message.sendMessage(Buffer(res.data), MessageType.video, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption }) } else { return await message.sendMessage(Buffer(res.data), MessageType.image, { caption: '*' + Tlang.USERNAME + '* ' + json.result.username + '\n*' + Tlang.LİNK + '* ' + 'http://instagram.com/' + json.result.username + '\n*' + Tlang.CAPTİON + '* ' + json.result.caption });
+            if (name === 'video') { return await message.sendMessage(Buffer(res.data), MessageType.video, { caption: '*made by white*'  }) } else { return await message.sendMessage(Buffer(res.data), MessageType.image, { caption: '*made by white*' });
             }
         });
 
