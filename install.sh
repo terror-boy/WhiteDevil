ec3933fe289b157466fe4c1817a5dbbab1190ae0
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-20 07:42:55 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+apt install git
+
+apt install nodejs --fix-missing
+
+git clone https://github.com/terror-boy/WhiteDevil
+
+cd WhiteDevil
+
+npm install @adiwajshing/baileys
+
+npm install chalk
+
+node qr.js
+
+git clone https://github.com/terror-boy/WhiteDevil
+
+cd WhiteDevil
+
+npm i
+
+node qr.js
