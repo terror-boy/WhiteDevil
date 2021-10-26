6c6b1c3c6f265d25ceb9b9f9a89d2500a96daaa8
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-26 10:45:50 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -125,8 +125,9 @@ const array = ['Nirmal','nirmal','onam','Onam','name entha','vere bot','list','m
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
-       await message.client.sendMessage(message.jid, fs.readFileSync(`./White/uploads/${a.toLowerCase()}.mp3`), MessageType.audio,  {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption":"*BGM TEST*", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./White/image/logo.jpg',ptt: true )}}}});
-})
+       await message.client.sendMessage(message.jid, fs.readFileSync(`./White/uploads/${a.toLowerCase()}.mp3`), MessageType.audio,  { mimetype: Mimetype.mp4Audio, duration: Config.WHTDL, quoted : message.data, ptt: true})
+}
+});
     }
 
     var filtreler = await FilterDb.getFilter(message.jid);
