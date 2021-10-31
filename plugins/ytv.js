6bb4979631c2e9b8644f92f28c37ccdbec1b2f43
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-10-31 15:43:06 +0530

    ```don't update```

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -7,7 +7,7 @@ const { MessageType, Mimetype } = require('@adiwajshing/baileys');
 const got = require('got');
 const Config = require('../config');
 const LOAD_ING = "*TRYING TO DOWNLOAD*"
-const UPLOAD_ING = "*✅️ DOWNLOADING COMPLETED \n\n UPLOADING IN PROCESS...*"
+const UPLOAD_ING = "*✅️ DOWNLOADING COMPLETED* \n\n *UPLOADING IN PROCESS...*"
 const axios = require('axios')
 const Axios = require('axios')
 
@@ -23,6 +23,8 @@ var reply = await message.client.sendMessage(message.jid, LOAD_ING , MessageType
         const { status, result } = data
 
 	var img = await Axios.get(`${result.thumbnail}`, {responseType: 'arraybuffer'})
+	
+	var video = await Axios.get(`${result.url_video}`, {responseType: 'arraybuffer'})
 
         if(!status) return await message.sendMessage('*NO RESULT FOUND🥲*')
 
@@ -36,5 +38,6 @@ var reply = await message.client.sendMessage(message.jid, LOAD_ING , MessageType
         msg +=  `DOWNLOADING LINK :${result.url_video}\n\n`
         msg += '```'
          return await message.client.sendMessage(message.jid,Buffer.from(img.data), MessageType.image, {mimetype: Mimetype.jpg , caption: msg })
+	return await message.client.sendMessage(message.jid,Buffer.from(video.data), MessageType.video, {mimetype: Mimetype.mp4Video , caption: msg })
         });
     
