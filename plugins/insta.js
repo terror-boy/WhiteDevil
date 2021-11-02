fae3da18c95b85fb5f7b5125a505a137f1ee2a2c
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-11-02 14:05:34 +0530

    Update insta.js

--- plugins/insta.js
+++ plugins/insta.js
@@ -7,6 +7,7 @@ WHITE DEVIL ----»»» TERROR BOY
 const Ktb = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const axios = require ('axios')
+const White = require('../white');
 const LOAD_ING = "*✅️Connecting To INSTAGRAM*"
 const UPLOAD_ING = "*✅️Connected To INSTAGRAM* \n\n\n *DOWNLOADING & UPLOADING IN PROCESS...*"
 const instagram = async (url, key) => {
@@ -21,9 +22,9 @@ Ktb.addCommand({pattern: 'insta ?(.*)', fromMe: false, desc: "Downloads from ins
 
     reply = await message.client.sendMessage(message.jid,UPLOAD_ING , MessageType.text, { quoted: message.data });
     
-    if (type === 'image') return await message.sendMessage(data, MessageType.image, { caption: "*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*", quoted: message.data })
+    if (type === 'image') return await message.sendMessage(data, MessageType.image, { caption: "*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*", quoted: message.data  , thumbnail: White.tm_b })
     
-    if (type === 'video') return await message.sendMessage(data, MessageType.video, { caption: "*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*", quoted: message.data })
+    if (type === 'video') return await message.sendMessage(data, MessageType.video, { caption: "*ᴍᴀᴅᴇ ʙʏ ᴡʜɪᴛᴇ ᴅᴇᴠɪʟ*", quoted: message.data , thumbnail: White.tm_b })
     
 });
     
