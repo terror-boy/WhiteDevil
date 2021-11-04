5a2452a1dd1331928431e5b4cddb69fd63d63023
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-11-04 10:07:27 +0530

    Update ktb.js

--- plugins/ktb.js
+++ plugins/ktb.js
@@ -16,6 +16,9 @@ const inquirer = require('inquirer')
 
 Asena.addCommand({pattern: 'ktb', fromMe: false, desc: "test"}, (async (message, match) => {
 
+    const { choice } = await inquirer.prompt(button);
+        choice == rows["rowid1"] && require("./git.js");
+        choice == rows["rowid2"] && require("./nokkatte.js");
 const rows = [
  {title: 'Github',  rowId:"rowid1"},
  {title: 'Owner',  rowId:"rowid2"}
@@ -30,9 +33,7 @@ const button = {
  listType: 1
 } 
 
-const { choice } = await inquirer.prompt(button);
-        choice == rows["rowid1"] && require("./git.js");
-        choice == rows["rowid2"] && require("./nokkatte.js");
+
     
         
 const sendMsg = await message.client.sendMessage(message.jid, button, MessageType.listMessage)
