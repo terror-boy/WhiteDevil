3f58e115af2d534f4475f6832f9a0c231c854ab4
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-11-06 23:05:05 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -7,12 +7,27 @@ WhiteDevil - Karthik_terror-boy
 */
 
 const chalk = require('chalk');
-const { WAConnection, MessageType } = require('@adiwajshing/baileys');
+const {WAConnection, MessageOptions, MessageType, Mimetype, Presence} = require('@adiwajshing/baileys');
 const fs = require('fs');
 async function whatsAsena() {
   const conn = new WAConnection();
   conn.logger.level = 'warn';
-  conn.version = [2, 2119, 6]
+  conn.version = [2,2142,12]
+  const rows = [
+ {title: 'Whitedevil bot support group:-', description: "\n\n*https://chat.whatsapp.com/KDT4Q5F4zq99ubruR5T9k1*", rowId:"wd1"},
+ {title: 'Password in heroku:-', description: "\n\n• whitedevil\n• Whitedevil\n\n\n```you can take any one```", rowId:"wd2"},
+ {title: 'All bot support group:-', description: "\n\n*https://chat.whatsapp.com/CY7wJC070o04yxfTuT1yPf*", rowId:"wd3"}
+]
+
+const sections = [{title: "please check it", rows: rows}]
+
+const button = {
+ buttonText: 'Click Me Here!',
+ description: "*Hello,Thank you for using Whitedevil*\n\n ```please check it```",
+ sections: sections,
+ listType: 1
+}
+
 
   conn.on('connecting', async () => {
     console.log(`${chalk.green.bold('Karthik_terror-boy ')}${chalk.green.bold('-WhiteDevil')}
@@ -20,7 +35,7 @@ ${chalk.white.italic('WhiteDevilString code recipient')}
 ${chalk.blue.bold('ℹ️  Connecting WhiteDevil... Please wait.')}`);
   });
 
-  conn.on('open', async () => {
+  conn.on('open', async () => { 
     console.log(
       chalk.green.bold('WHITEDEVIL QR Code: '),
       'WHITE;;;' +
@@ -42,10 +57,26 @@ ${chalk.blue.bold('ℹ️  Connecting WhiteDevil... Please wait.')}`);
         '*~___________~* *'+ conn.user.name + ' ~___________~*\n\n*▪️ WhiteDevil Successfully Scanned✅️*\n*▪️Thanks For Choosing WhiteDevil 💞*',
         MessageType.text
       );
+      await conn.sendMessage(
+        conn.user.jid, fs.readFileSync("./boot/bott.mp3"), MessageType.audio, { mimetype: Mimetype.mp4Audio, ptt: true});
+        await conn.sendMessage( conn.user.jid, button, MessageType.listMessage);
+        await conn.sendMessage(
+        conn.user.jid,
+        '*~___________~* *'+ conn.user.name + ' ~___________~*\n\n*THIS IS ANNOUNCEMENT MESSAGE*\n\n*THERE WILL BE NO REPLY FROM MY OWNWER FOR MONDAY-FRIDAY BECAUSE OF BUSY IN CLASS* \n\n *YOU CAN REPORT BUGS & ERRORS IN OFFICIAL BOT GROUP[ https://chat.whatsapp.com/CY7wJC070o04yxfTuT1yPf ]* \n*FOR ALL BOT SUPPORT* \n *YOU SHOULD MENTION ME IN THIS GROUP OR SAY THE NAME OF BOT.......*\n\n\n\n           --- *BOT OWNER*',
+        MessageType.text
+      );
     } else {
       await conn.sendMessage(
         conn.user.jid,
-        '*~_____________~* *'+ conn.user.name + ' ~_____________~*\n\n*▪️ WhiteDevil Successfully Scanned✅️*\n*▪️Thanks For Choosing WhiteDevil 💞*',
+        '*~__~* *'+ conn.user.name + ' ~__~*\n\n*▪️ WhiteDevil Successfully Scanned✅️*\n*▪️Thanks For Choosing WhiteDevil 💞*',
+        MessageType.text
+      );
+      await conn.sendMessage(
+        conn.user.jid, fs.readFileSync("./boot/bott.mp3"), MessageType.audio, { mimetype: Mimetype.mp4Audio, ptt: true});
+        await conn.sendMessage( conn.user.jid, button, MessageType.listMessage);
+        await conn.sendMessage(
+        conn.user.jid,
+        '*~__~* *'+ conn.user.name + ' ~__~*\n\n*THIS IS ANNOUNCEMENT MESSAGE*\n\n*THERE WILL BE NO REPLY FROM MY OWNWER FOR MONDAY-FRIDAY BECAUSE OF BUSY IN CLASS* \n\n *YOU CAN REPORT BUGS & ERRORS IN OFFICIAL BOT GROUP[ https://chat.whatsapp.com/CY7wJC070o04yxfTuT1yPf ]* \n*FOR ALL BOT SUPPORT* \n *YOU SHOULD MENTION ME IN THIS GROUP OR SAY THE NAME OF BOT.......*\n\n\n\n           --- *BOT OWNER*',
         MessageType.text
       );
     }
