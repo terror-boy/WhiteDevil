9fd17c0efef1f5826ed231488888b2b0f71aba38
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-11-15 22:49:38 +0530

    ```more to fix```

--- plugins/waconnect.js
+++ plugins/waconnect.js
@@ -4,7 +4,7 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
 WhiteDevil - Karthik_terror-boy
-*/
+
 
 const Ktb = require('../events');
 
@@ -102,3 +102,4 @@ ${chalk.blue.bold('ℹ️  Connecting WhiteDevil... Please wait.')}`);
 
 
 
+*/
