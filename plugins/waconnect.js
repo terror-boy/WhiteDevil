c731683ef6dd551d86bac1332a5bd277249bf09e
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-11-20 21:06:19 +0530

    Update waconnect.js

--- plugins/waconnect.js
+++ plugins/waconnect.js
@@ -4,13 +4,14 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
 WhiteDevil - Karthik_terror-boy
-
+*/
 
 const Ktb = require('../events');
 
 const chalk = require('chalk');
 const {WAConnection, MessageOptions, MessageType, Mimetype, Presence} = require('@adiwajshing/baileys');
 
+const qrcode = require('qrcode')
 const fs = require('fs');
 
     Ktb.addCommand({pattern: 'tmp$', fromMe: true, desc: 'testing'}, (async (message, match) => {   
@@ -18,37 +19,18 @@ const fs = require('fs');
   const conn = new WAConnection();
   conn.logger.level = 'warn';
   conn.version = [3, 3234, 9]
-  const rows = [
- {title: 'Whitedevil bot support group:-', description: "\n\n*https://chat.whatsapp.com/KDT4Q5F4zq99ubruR5T9k1*", rowId:"wd1"},
- {title: 'Password in heroku:-', description: "\n\n• whitedevil\n• Whitedevil\n\n\n```you can take any one```", rowId:"wd2"},
- {title: 'All bot support group:-', description: "\n\n*https://chat.whatsapp.com/CY7wJC070o04yxfTuT1yPf*", rowId:"wd3"}
-]
-
-const sections = [{title: "please check it", rows: rows}]
-
-const button = {
- buttonText: 'Click Me Here!',
- description: "*Hello,Thank you for using Whitedevil*\n\n ```please check it```",
- sections: sections,
- listType: 1
-}
-
-
-  conn.on('connecting', async () => {
-    console.log(`${chalk.green.bold('Karthik_terror-boy ')}${chalk.green.bold('-WhiteDevil')}
-${chalk.white.italic('WhiteDevilString code recipient')}
-${chalk.blue.bold('ℹ️  Connecting WhiteDevil... Please wait.')}`);
-  });
-
-  conn.on('open', async () => { 
-    console.log(
-      chalk.green.bold('WHITEDEVIL QR Code: '),
-      'WHITE;;;' +
-      Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
-        'base64'
-      )
-    );
-    await conn.sendMessage(
+ conn.on('qr', async qr => {
+    	let bot = await qrcode.toDataURL(qr, { scale: 8 })
+    	let buffer = new Buffer.from(bot.replace('data:image/png;base64,', ''), 'base64')
+       	bot = await client.sendMessage(id,buffer,MessageType.image,{caption:'Scan QR to become a bot\n*Rules:*\nQR will be changed every 30 seconds'})
+    	setTimeout(() => {
+       	client.deleteMessage(id, bot.key)
+       },30000)
+    })
+    conn.on('connecting', () => {
+    })
+    conn.on('open', () => {
+    	await conn.sendMessage(
       conn.user.jid,
       'WHITE;;;' +
       Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
@@ -56,50 +38,7 @@ ${chalk.blue.bold('ℹ️  Connecting WhiteDevil... Please wait.')}`);
       ),
       MessageType.text
     );
-    if (conn.user.jid.startsWith('91')) {
-      await conn.sendMessage(
-        conn.user.jid,
-        '*~___________~* *'+ conn.user.name + ' ~___________~*\n\n*▪️ WhiteDevil Successfully Scanned✅️*\n*▪️Thanks For Choosing WhiteDevil 💞*',
-        MessageType.text
-      );
-      await conn.sendMessage(
-        conn.user.jid, fs.readFileSync("./bott.mp3"), MessageType.audio, { mimetype: Mimetype.mp4Audio, ptt: true});
-        await conn.sendMessage( conn.user.jid, button, MessageType.listMessage);
-        await conn.sendMessage(
-        conn.user.jid,
-        '*~___________~* *'+ conn.user.name + ' ~___________~*\n\n*THIS IS ANNOUNCEMENT MESSAGE*\n\n*THERE WILL BE NO REPLY FROM MY OWNWER FOR MONDAY-FRIDAY BECAUSE OF BUSY IN CLASS* \n\n *YOU CAN REPORT BUGS & ERRORS IN OFFICIAL BOT GROUP[ https://chat.whatsapp.com/CY7wJC070o04yxfTuT1yPf ]* \n*FOR ALL BOT SUPPORT* \n *YOU SHOULD MENTION ME IN THIS GROUP OR SAY THE NAME OF BOT.......*\n\n\n\n           --- *BOT OWNER*',
-        MessageType.text
-      );
-    } else {
-      await conn.sendMessage(
-        conn.user.jid,
-        '*~__~* *'+ conn.user.name + ' ~__~*\n\n*▪️ WhiteDevil Successfully Scanned✅️*\n*▪️Thanks For Choosing WhiteDevil 💞*',
-        MessageType.text
-      );
-      await conn.sendMessage(
-        conn.user.jid, fs.readFileSync("./bott.mp3"), MessageType.audio, { mimetype: Mimetype.mp4Audio, ptt: true});
-        await conn.sendMessage( conn.user.jid, button, MessageType.listMessage);
-        await conn.sendMessage(
-        conn.user.jid,
-        '*~__~* *'+ conn.user.name + ' ~__~*\n\n*THIS IS ANNOUNCEMENT MESSAGE*\n\n*THERE WILL BE NO REPLY FROM MY OWNWER FOR MONDAY-FRIDAY BECAUSE OF BUSY IN CLASS* \n\n *YOU CAN REPORT BUGS & ERRORS IN OFFICIAL BOT GROUP[ https://chat.whatsapp.com/CY7wJC070o04yxfTuT1yPf ]* \n*FOR ALL BOT SUPPORT* \n *YOU SHOULD MENTION ME IN THIS GROUP OR SAY THE NAME OF BOT.......*\n\n\n\n           --- *BOT OWNER*',
-        MessageType.text
-      );
-    }
-    console.log(
-      chalk.green.bold(
-        "\n\n Nigalkku sandesham pakarthan \n kaliyunnillegil,whatsapp parishodikkuka \n nigalude numberillekku  code ayachinnu!\n\n\n\n"
-      ),
-      chalk.green.bold(
-        'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!'
-      )
-    );
-    process.exit(0);
-  });
-
-  await conn.connect()
-}))
 
 
 
 
-*/
