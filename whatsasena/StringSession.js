cca417593832a5effb66e9ed64159e3be2782659
farooqbot <85386300+farooqbot@users.noreply.github.com>
2021-11-23 11:11:29 +0530

    Update StringSession.js

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('WHITE_SESSION' in process.env && string === undefined) {
+        if ('Farooq_SESSION' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'WHITE;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'Farooq;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
