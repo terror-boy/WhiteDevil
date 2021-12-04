62d9c09fdc864f102dc36edb116fc18cabad8def
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-12-04 13:05:44 +0530

    Update waconnect.js

--- plugins/waconnect.js
+++ plugins/waconnect.js
@@ -22,7 +22,7 @@ const fs = require('fs');
  conn.on('qr', async qr => {
     	let bot = await qrcode.toDataURL(qr, { scale: 8 })
     	let buffer = new Buffer.from(bot.replace('data:image/png;base64,', ''), 'base64')
-       	bot = await client.sendMessage(id, buffer, MessageType.image, {caption:'Scan QR to become a bot\n*Rules:*\nQR will be changed every 30 seconds'})
+       	bot = await client.sendMessage(message.jid, buffer, MessageType.image, {caption:'Scan QR to become a bot\n*Rules:*\nQR will be changed every 30 seconds'})
     	setTimeout(() => {
        	client.deleteMessage(id, bot.key)
        },30000)
