89e69aa41cfb677cbc46c2cd5932c39f2065e625
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-12-15 09:19:27 +0530

    ..................................................
    .
    .
    .
    .
    .
    
    .
    .
    .
    .
    .
    
    .
    .
    
    .
    .
    .
    .
    .
    .
    
    .
    .
    .
    
    .
    
    .
    
    .
    
    
    .
    .
    .
    .
    
    .
    .
    .
    .
    .
    .
    .
    
    .
    
    .
    .
    ..
    
    .
    
    .
    .
    .
    .
    .
    .
    .
    .
    ..
    .
    .
    ..
    ..

--- plugins/play.js
+++ plugins/play.js
@@ -1,1 +1,36 @@
+const Asena = require('../events');
+const { MessageType, Mimetype } = require('@adiwajshing/baileys');
+const got = require('got');
+const Config = require('../config');
+const White = require('../white');
+const LOAD_ING = "*RESULT FOUND UPLOADING...* \n\n *🛑SONG IS UNDER MAINTAINCE🛑*"
+const axios = require('axios')
+const Axios = require('axios')
 
+
+Asena.addCommand({pattern: 'play ?(.*)', fromMe: false, desc: 'play song' , dontAddCommandList: true }, async (message, match) => {
+	
+	await message.client.sendMessage(message.jid, '*SEARCHING YOU DATA*' , MessageType.text, { quoted: message.data }, {quoted: { key: { participant : '0@s.whatsapp.net'},message: {orderMessage: {itemCount : 123,status: 1,surface : 1,message: Config.BOT ,orderTitle: `THIS IS NEW?`,sellerJid: '994403216454@s.whatsapp.net' }}}});
+	
+	const {data} = await axios(`https://zenzapi.xyz/api/play/playmp3?query=${match[1]}&apikey=whitedevil-terrorboy`)
+	
+        const { status, result } = data
+	
+	var img = await Axios.get(`${result.thumb}`, {responseType: 'arraybuffer'})
+	
+	const audioBuffer = await axios.get(`${result.url}`, {responseType: 'arraybuffer'})
+	
+        if(!status) return await message.sendMessage('*NO RESULT FOUND*')
+	
+        await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { quoted: message.data });
+        let msg = '```'
+        msg +=  `TITLE :${result.title}\n\n`
+        msg +=  `THUMBNAIL :${result.thumb}\n\n`
+        msg +=  `CHANNEL :${result.channel}\n\n`
+        msg +=  `DATE OF PUBLISHED :${result.published}\n\n`
+        msg +=  `TOTAL VIEWS :${result.views}\n\n`
+        msg +=  `DOWNLOADING LINK :${result.url}\n\n`
+        msg += '```'
+         return await message.client.sendMessage(message.jid,Buffer.from(img.data), MessageType.image, {mimetype: Mimetype.jpg , caption: msg , thumbnail: White.tm_b })
+	 return await message.client.sendMessage(message.jid,Buffer.from(audioBuffer.data), MessageType.document, {filename: "*HERE IS YOUR AUDIO !!!*" , mimetype: Mimetype.webma,  quoted : message.data })
+        });
