6fdab4537a42548197221d9eab337a174581479d
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-12-24 20:46:54 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -24,57 +24,65 @@ const Lang = Language.getString('system_stats');
 const conf = require('../config');
 let wk = conf.WORKTYPE == 'public' ? false : true
 
-  Ktb.addCommand({pattern: 'alive', fromMe: wk, desc: Lang.ALIVE_DESC}, (async (message, match) => {
-    //  ....................................................................................................
+var SYSDTXT =  '💻 System status'
+
+var VER = '🧬 Version'
+//  ....................................................................................................
       var plk_say = new Date().toLocaleString('HI', { timeZone: 'Asia/Kolkata' }).split(' ')[1]           // .
         const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };   // .   
         var plk_here = new Date().toLocaleDateString(get_localized_date)                                  // .  --------->>> CREDIT:-AFNAN PLK PINKYMWOL 
 	  var afnplk = '```⏱ Time :' + plk_say + '```\n\n ```📅 Date :' + plk_here + '```'                  // .
                                                                                                           // . 
     // ..................................................................................................  
-        let pp
-        try { pp = await message.client.getProfilePicture(message.jid.includes('-') ? message.data.participant : message.jid ); } catch { pp = await message.client.getProfilePicture(); }
-        await axios.get(pp, {responseType: 'arraybuffer'}).then(async (res) => { await message.client.sendMessage(message.jid, res.data, MessageType.image,{ quoted: message.data, thumbnail: White.tm_b ,caption: Config.ALIVEMSG.replace('{pp}', '').replace('{time}', afnplk).replace('{qt}' )})}); 
+var MSG = config.ALIVEMSG.replace('{time}', afnplk).replace('{qt}' )
+
+  Ktb.addCommand({pattern: 'alive', fromMe: wk, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+    
+    var logo = await axios.get ('https://i.imgur.com/GerJ77X.mp4', {responseType: 'arraybuffer'})
+    var PIC = Buffer.from(logo.data)
+
+    const media = await message.client.prepareMessage(message.jid, PIC, MessageType.video, { thumbnail: White.tm_b })
+
+    var BUTTHANDLE = '';
+    if (/\[(\W*)\]/.test(config.HANDLERS)) {
+        BUTTHANDLE = config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+    } else {
+        BUTTHANDLE = '.';
+    }
+        
+    const buttons = [
+        {buttonId: BUTTHANDLE + 'version', buttonText: {displayText: VER }, type: 1},
+        {buttonId: BUTTHANDLE + 'status', buttonText: {displayText: SYSDTXT }, type: 1}
+    ]
+    const buttonMessage = {
+        contentText: MSG,
+        footerText: '© Terror-boy ©',
+        buttons: buttons,
+        headerType: 5,
+        videoMessage: media.message.videoMessage  
+    }
+    await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage,{ quoted: message.data});
+}))
+
+Ktb.addCommand({pattern: 'status', fromMe: wk, desc: Lang.SYSD_DESC, dontAddCommandList: true,  deleteCommand: false}, (async (message, match) => {
+    
+    const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+    await message.sendMessage(
+        '```' + child + '```', MessageType.text, {quoted: message.data}
+    );
+}));
+
+Ktb.addCommand({pattern: 'version', fromMe: wk, desc: 'nil', dontAddCommandList: true,  deleteCommand: false}, (async (message, match) => {
+   
+    await message.client.sendMessage(message.jid, 
+        `*🧬 WhiteDevil Version 🧬*\n\n` + 
+        '```Installed version :```\n' +
+        config.VERSION + 
+        `\n\nCheck official website : https://whitedevil-bot.yolasite.com/`
+   , MessageType.text, {quoted: message.data});
+    
 }));
 
      
      
-     
-    Ktb.addCommand({pattern: 'sysd', fromMe: wk, desc: Lang.SYSD_DESC}, (async (message, match) => {
-
-        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
-        await message.sendMessage(
-            '```' + child + '```', MessageType.text
-        );
-    }));
-
-
-
-Ktb.addCommand({pattern: 'al', fromMe: wk, desc: Lang.ALIVE_DESC}, (async (message, match) => {
-
-             const buttons = [
-  {buttonId: '!git', buttonText: {displayText: 'chumma'}, type: '!git'},
-  {buttonId: 'owner', buttonText: {displayText: '😀😀😀'}, type: 'owner'}
-]
-
-const buttonMessage = {
-    contentText: 'hi',
-    footerText: '© ktb ©',
-    buttons: buttons,
-    headerType: "EMPTY"
-}
-
-   await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage)})); 
-
-
-
-    
-    Ktb.addCommand({pattern: 'njn', fromMe: wk, desc: Lang.SYSD_DESC, dontAddCommandList: true }, (async (message, match) => {
-
-        
-        await message.sendMessage(message.jid, "I'm " +conn.user.name+'\n Wa version :' +conn.user.phone.wa_version+'\n Version :' + Config.VERSION +'\n Phone Version :' + conn.user.phone.os_version+'\n Device Model :' +conn.user.phone.device_model+ '\n Phone Manufacturer :' +conn.user.phone.device_manufacturer  , MessageType.text);
-    
-                                                                           
-
-    
-    }))
+   
