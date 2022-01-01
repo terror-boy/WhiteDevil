31e889b43b057f3445fd85fb531a281374dfc97b
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2022-01-01 19:10:00 +0530

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,1 +1,67 @@
+/* Coded by rashi
+Re-coded by KTB 
+*/
+
+const Asena = require('../events');
+const { MessageType, Mimetype } = require('@adiwajshing/baileys');
+const got = require('got');
+const Config = require('../config');
+const White = require('../white');
+const LOAD_ING = "*TRYING TO DOWNLOAD*"
+const UPLOAD_ING = "*✅️ DOWNLOADING COMPLETED* \n\n *UPLOADING IN PROCESS...*"
+const axios = require('axios')
+const Axios = require('axios')
+
+const options = {}
+
+const conf = require('../config');
+let wk = conf.WORKTYPE == 'public' ? false : true
+
+Asena.addCommand({pattern: 'ytv ?(.*)', fromMe: wk, desc: 'video downloading links from youtube'}, async (message, match) => {
+	
+const ig = await Axios.get('https://avatars.githubusercontent.com/u/85664936?s=120&v=4', {responseType: 'arraybuffer'})	
+options.linkPreview = {
+               head: "WhiteDevil",
+               body: "© TERROR-BOY ©",
+               mediaType: 2, //3 for video
+               thumbnail: Buffer.from(ig.data) ,
+               sourceUrl: "https://github.com/terror-boy/WhiteDevil",
+                }
+
+	options.quoted = {
+            key: {
+                fromMe: false,
+                participant: "0@s.whatsapp.net",
+                remoteJid: "status@broadcast"
+            },
+            message: {
+                "imageMessage": {
+                    "jpegThumbnail": Buffer.from(ig.data),
+                    "caption": "ιтѕ мє  WhiteDevil"
+                }
+            }
+        }
+var reply = await message.client.sendMessage(message.jid, LOAD_ING , MessageType.text, { options});
+
+        const {data} = await axios(`https://api.zeks.me/api/ytplaymp4?apikey=ApiKannappi&q=${match[1]}`)
+	
+        const { status, result } = data
+
+
+	const videoBuffer = await axios.get(`${result.url_video}`, {responseType: 'arraybuffer'})
+
+        if(!status) return await message.sendMessage('*NO RESULT FOUND🥲*')
+
+	reply = await message.client.sendMessage(message.jid,UPLOAD_ING , MessageType.text, { options});
+
+        let msg = '```'
+        msg +=  `TITLE :${result.title}\n\n`
+        msg +=  `THUMBNAIL :${result.thumbnail}\n\n`
+        msg +=  `SOURCE :${result.source}\n\n`
+        msg +=  `SIZE :${result.size}\n\n`
+        msg +=  `DOWNLOADING LINK :${result.url_video}\n\n`
+        msg += '```' 
+	 return await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {mimetype: Mimetype.mp4, ptt: false , caption: msg , thumbnail: White.tm_b}); 
+});
+    
 
